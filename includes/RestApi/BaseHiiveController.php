be1cf6cc9f752cb4f839f1077c96dc32b0f99549
arun.sh <arun.sh@endurance.com>
2022-05-23 16:01:44 +0530

    PRESS2-23 create API to retrieve site images given a site type

---
+++ includes/RestApi/BaseHiiveController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace NewfoldLabs\WP\Module\Onboarding\RestApi;
+
+/**
+ * Base class for Hiive related actions.
+ */
+abstract class BaseHiiveController extends \WP_REST_Controller {
+
+    /**
+     * @var string
+     */
+    protected $url = 'https://hiive.cloud';
+
+    /**
+     * @param string $endpoint
+     * @param array $args
+     * 
+     * @return WP_Error|string containing the Hiive response.
+     */
+    protected function get ( $endpoint, $args = array() ) {
+        $request = $this->url . $endpoint . '?' . http_build_query( $args );
+
+        $response = \wp_remote_get( $request );
+        if ( 200 === \wp_remote_retrieve_response_code( $response ) ) {
+            $payload = \wp_remote_retrieve_body( $response );
+
+            return $payload;
+        }
+
+        return new \WP_Error(
+            'hiive-error',
+            'Error in fetching data.',
+            array( 'status' => \wp_remote_retrieve_response_code( $response ) )
+        );
+    }
+}
