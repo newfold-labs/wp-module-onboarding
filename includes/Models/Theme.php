18e878f7a3c47e7633d5a456c396c234711fbefa
arunshenoy99 <devarunshenoy99@gmail.com>
2022-06-03 16:28:19 +0530

    PRESS2-12 fix formatting with phpcbf

--- includes/Models/Theme.php
+++ includes/Models/Theme.php
@@ -7,75 +7,74 @@ namespace NewfoldLabs\WP\Module\Onboarding\Models;
  */
 class Theme implements \JsonSerializable {
 
-    private string $theme_name;
-    private string $theme_image;
-    private bool $is_newfold_theme;
+	private string $theme_name;
+	private string $theme_image;
+	private bool $is_newfold_theme;
 
-    /**
-     * @param string $theme_name
-     */
-    public function __construct( $theme_name ) 
-    {
-        $this->theme_name = $theme_name;
-        $this->is_newfold_theme = false;
-    }
+	/**
+	 * @param string $theme_name
+	 */
+	public function __construct( $theme_name ) {
+		$this->theme_name       = $theme_name;
+		$this->is_newfold_theme = false;
+	}
 
-    /**
-     * @param string $theme_name
-     * 
-     * @return void
-     */
-    public function set_theme_name( $theme_name ) {
-        $this->theme_name = $theme_name;
-    }
+	/**
+	 * @param string $theme_name
+	 *
+	 * @return void
+	 */
+	public function set_theme_name( $theme_name ) {
+		$this->theme_name = $theme_name;
+	}
 
-    /**
-     * @return string $theme_name
-     */
-    public function get_theme_name() {
-        return $this->theme_name;
-    }
+	/**
+	 * @return string $theme_name
+	 */
+	public function get_theme_name() {
+		return $this->theme_name;
+	}
 
-    /**
-     * @param string $theme_image Path to theme screenshot image.
-     * 
-     * @return void
-     */
-    public function set_theme_image( $theme_image ) {
-        $this->theme_image = $theme_image;
-    }
+	/**
+	 * @param string $theme_image Path to theme screenshot image.
+	 *
+	 * @return void
+	 */
+	public function set_theme_image( $theme_image ) {
+		$this->theme_image = $theme_image;
+	}
 
-    /**
-     * @return $theme_image Path to theme screenshot image.
-     */
-    public function get_theme_image() {
-        return $this->theme_image;
-    }
+	/**
+	 * @return $theme_image Path to theme screenshot image.
+	 */
+	public function get_theme_image() {
+		return $this->theme_image;
+	}
 
-    /**
-     * @param boolean $is_newfold_theme
-     * 
-     * @return void
-     */
-    public function set_is_newfold_theme( $is_newfold_theme ) {
-        $this->is_newfold_theme = $is_newfold_theme;
-    }
+	/**
+	 * @param boolean $is_newfold_theme
+	 *
+	 * @return void
+	 */
+	public function set_is_newfold_theme( $is_newfold_theme ) {
+		$this->is_newfold_theme = $is_newfold_theme;
+	}
 
-    /**
-     * @return boolean $is_newfold_theme true if the theme author is Newfold Digital.
-     */
-    public function get_is_newfold_theme() {
-        return $this->is_newfold_theme;
-    }
+	/**
+	 * @return boolean $is_newfold_theme true if the theme author is Newfold Digital.
+	 */
+	public function get_is_newfold_theme() {
+		return $this->is_newfold_theme;
+	}
 
-    /**
-     * @return array JSON Serialize the data
-     */
-    public function jsonSerialize() {
-        return array(
-            'name'           => $this->theme_name,
-            'image'          => $this->theme_image,
-            'isNewfoldTheme' => $this->is_newfold_theme
-        );
-    }
+	/**
+	 * @return array JSON Serialize the data
+	 */
+	public function jsonSerialize() {
+		return array(
+			'name'           => $this->theme_name,
+			'image'          => $this->theme_image,
+			'isNewfoldTheme' => $this->is_newfold_theme,
+		);
+	}
 }
--- includes/RestApi/Themes/ApprovedThemesController.php
+++ includes/RestApi/Themes/ApprovedThemesController.php
@@ -9,111 +9,112 @@ use NewfoldLabs\WP\Module\Onboarding\Permissions;
  */
 class ApprovedThemesController extends \WP_REST_Controller {
 
-    /**
-     * The namespace of this controller's route.
-     *
-     * @var string
-     */
-    protected $namespace = 'newfold-onboarding/v1';
-	    
-    /**
-     * The base of this controller's route.
-     *
-     * @var string
-     */
-    protected $rest_base = 'themes';
+	/**
+	 * The namespace of this controller's route.
+	 *
+	 * @var string
+	 */
+	protected $namespace = 'newfold-onboarding/v1';
 
-    /**
-     * Registers the settings route
-     */
-    public function register_routes() {
-        \register_rest_route(
-            $this->namespace,
-            '/'. $this->rest_base,
-            array(
-                array(
-                    'methods'             => \WP_REST_Server::READABLE,
-                    'callback'            => array( $this, 'get_approved_themes' ),
-                    'permission_callback' => array( $this, 'check_permission' ),
-                )
-            )
-        );
-    }
+	/**
+	 * The base of this controller's route.
+	 *
+	 * @var string
+	 */
+	protected $rest_base = 'themes';
 
-    /**
-     * Get a list of approved themes.
-     *
-     * [TODO] Retrieve the list from Hiive.
-     * 
-     * @return array|\WP_Error
-     */
-    protected function get_approved_theme_slugs() {
+	/**
+	 * Registers the settings route
+	 */
+	public function register_routes() {
+		\register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base,
+			array(
+				array(
+					'methods'             => \WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_approved_themes' ),
+					'permission_callback' => array( $this, 'check_permission' ),
+				),
+			)
+		);
+	}
 
-        // Key-Value array implementation of approved themes for faster lookup
-        return array( 'twentytwentytwo' => 1 );
-    }
+	/**
+	 * Get a list of approved themes.
+	 *
+	 * [TODO] Retrieve the list from Hiive.
+	 *
+	 * @return array|\WP_Error
+	 */
+	protected function get_approved_theme_slugs() {
 
-    /**
-     * Retrieves list of installed themes.
-     *
-     * @return array|\WP_Error
-     */
-    public function get_installed_themes() {
+		// Key-Value array implementation of approved themes for faster lookup
+		return array( 'twentytwentytwo' => 1 );
+	}
 
-        $request = new \WP_REST_Request(
-            'GET',
-            '/wp/v2/' . $this->rest_base
-        );
-        $response = \rest_do_request( $request );
+	/**
+	 * Retrieves list of installed themes.
+	 *
+	 * @return array|\WP_Error
+	 */
+	public function get_installed_themes() {
 
-        if (200 === $response->status) {
-            $themes_data = json_decode( \wp_json_encode( $response->data ), true );
-            foreach( $themes_data as $theme_data ) {
-                $theme = new Theme( $theme_data['template'] );
-                $theme->set_theme_image( $theme_data['screenshot'] );
-                if( str_contains( $theme_data['author']['raw'], 'Newfold Digital' ) )
-                    $theme->set_is_newfold_theme( true );
-                $installed_themes[] = $theme;
-            }
+		$request  = new \WP_REST_Request(
+			'GET',
+			'/wp/v2/' . $this->rest_base
+		);
+		$response = \rest_do_request( $request );
 
-            return $installed_themes;
-        }
+		if ( 200 === $response->status ) {
+			$themes_data = json_decode( \wp_json_encode( $response->data ), true );
+			foreach ( $themes_data as $theme_data ) {
+				$theme = new Theme( $theme_data['template'] );
+				$theme->set_theme_image( $theme_data['screenshot'] );
+				if ( str_contains( $theme_data['author']['raw'], 'Newfold Digital' ) ) {
+					$theme->set_is_newfold_theme( true );
+				}
+				$installed_themes[] = $theme;
+			}
 
-        return new \WP_Error(
-            $response->status,
-            'Failed to fetch installed themes.'
-        );
-    }
+			return $installed_themes;
+		}
 
-    /**
-     * Retrieves the themes offered by the Onboarding Module.
-     *
-     * @return \WP_REST_RESPONSE|\WP_Error
-     */
-    public function get_approved_themes() {
-        $approved_theme_slugs = $this->get_approved_theme_slugs();
-        $installed_themes     = $this->get_installed_themes();
+		return new \WP_Error(
+			$response->status,
+			'Failed to fetch installed themes.'
+		);
+	}
 
-        if( \is_wp_error( $installed_themes ) ) {
-            return $installed_themes;
-        }
+	/**
+	 * Retrieves the themes offered by the Onboarding Module.
+	 *
+	 * @return \WP_REST_RESPONSE|\WP_Error
+	 */
+	public function get_approved_themes() {
+		$approved_theme_slugs = $this->get_approved_theme_slugs();
+		$installed_themes     = $this->get_installed_themes();
 
-        foreach ( $installed_themes as $installed_theme ) {
-            if( $installed_theme->get_is_newfold_theme() 
-                || array_key_exists( $installed_theme->get_theme_name(), $approved_theme_slugs ) ) {
-                $approved_themes[] = $installed_theme;
-            }
-        }
+		if ( \is_wp_error( $installed_themes ) ) {
+			return $installed_themes;
+		}
 
-        return $approved_themes;
-    }
+		foreach ( $installed_themes as $installed_theme ) {
+			if ( $installed_theme->get_is_newfold_theme()
+				|| array_key_exists( $installed_theme->get_theme_name(), $approved_theme_slugs ) ) {
+				$approved_themes[] = $installed_theme;
+			}
+		}
 
-    /**
-     * Check permissions for routes.
-     *
-     * @return bool|\WP_Error
-     */
-    public function check_permission() {
-        return Permissions::rest_is_authorized_admin();
-    }
+		return $approved_themes;
+	}
+
+	/**
+	 * Check permissions for routes.
+	 *
+	 * @return bool|\WP_Error
+	 */
+	public function check_permission() {
+		return Permissions::rest_is_authorized_admin();
+	}
 }
