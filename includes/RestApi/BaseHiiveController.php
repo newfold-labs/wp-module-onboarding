397d3ed4fa0b1b7689272ca7f679ef36f229073d
Abhijit Bhatnagar <abhijit.bhatnagar@gmail.com>
2022-06-14 18:00:18 +0530

    Merged Changes from trunk into add/PRESS2-9-onboarding-data-structure

--- includes/RestApi/BaseHiiveController.php
+++ includes/RestApi/BaseHiiveController.php
@@ -7,40 +7,40 @@ namespace NewfoldLabs\WP\Module\Onboarding\RestApi;
  */
 abstract class BaseHiiveController extends \WP_REST_Controller {
 
-    /**
-     * @var string
-     */
-    protected $url;
+	/**
+	 * @var string
+	 */
+	protected $url;
 
-    public function __construct() {
+	public function __construct() {
 
-        if ( ! defined( 'NFD_HIIVE_BASE_URL' ) ) {
-            define( 'NFD_HIIVE_BASE_URL', 'https://hiive.cloud');
-        }
+		if ( ! defined( 'NFD_HIIVE_BASE_URL' ) ) {
+			define( 'NFD_HIIVE_BASE_URL', 'https://hiive.cloud' );
+		}
 
-        $this->url = NFD_HIIVE_BASE_URL;
-    }
+		$this->url = NFD_HIIVE_BASE_URL;
+	}
 
-    /**
-     * @param string $endpoint
-     * @param array $args
-     * 
-     * @return WP_Error|string containing the Hiive response.
-     */
-    protected function get ( $endpoint, $args = array() ) {
-        $request = $this->url . $endpoint . '?' . http_build_query( $args );
+	/**
+	 * @param string $endpoint
+	 * @param array  $args
+	 *
+	 * @return WP_Error|string containing the Hiive response.
+	 */
+	protected function get( $endpoint, $args = array() ) {
+		$request = $this->url . $endpoint . '?' . http_build_query( $args );
 
-        $response = \wp_remote_get( $request );
-        if ( 200 === \wp_remote_retrieve_response_code( $response ) ) {
-            $payload = \wp_remote_retrieve_body( $response );
+		$response = \wp_remote_get( $request );
+		if ( 200 === \wp_remote_retrieve_response_code( $response ) ) {
+			$payload = \wp_remote_retrieve_body( $response );
 
-            return $payload;
-        }
+			return $payload;
+		}
 
-        return new \WP_Error(
-            'hiive-error',
-            'Error in fetching data.',
-            array( 'status' => \wp_remote_retrieve_response_code( $response ) )
-        );
-    }
+		return new \WP_Error(
+			'hiive-error',
+			'Error in fetching data.',
+			array( 'status' => \wp_remote_retrieve_response_code( $response ) )
+		);
+	}
 }
