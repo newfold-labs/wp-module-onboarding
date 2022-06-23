37d99bbab4071904935bbd6feb69d9b10dfa362e
Abhijit Bhatnagar <abhijit.bhatnagar@gmail.com>
2022-06-23 12:56:15 +0530

    Merged changes from trunk into add/PRESS2-25-custom-post-objects-endpoint

--- includes/RestApi/RestApi.php
+++ includes/RestApi/RestApi.php
@@ -9,12 +9,14 @@ final class RestApi {
 
 	protected $controllers = array(
 		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\SiteImagesController',
+		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\FlowController',
 		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\PluginsController',
 		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\Themes\\ThemeVariationsController',
-		'NewfoldLabs\WP\\Module\\Onboarding\\RestApi\\Themes\\ApprovedThemesController',
-          'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\PatternsController',
-          'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\PagesController',
-		);
+		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\Themes\\ApprovedThemesController',
+		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\PatternsController',
+		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\SettingsController',
+		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\PagesController',
+	);
 
 	public function __construct() {
 		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
