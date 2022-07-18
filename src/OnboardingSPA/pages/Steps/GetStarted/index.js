585afce4acb7d6059bf357c19e720e26beb8e8b7
Yashita <yashita.jain@newfold.com>
2022-07-18 15:31:23 +0530

    Update index.js

--- src/OnboardingSPA/pages/Steps/GetStarted/index.js
+++ src/OnboardingSPA/pages/Steps/GetStarted/index.js
@@ -4,7 +4,7 @@ import { useEffect } from '@wordpress/element';
 import { useLocation } from 'react-router-dom';
 import { useDispatch } from '@wordpress/data';
 import NewfoldLargeCard from '../../../components/NewfoldLargeCard';
-import { VIEW_NAV_GET_STARTED, VIEW_NAV_PRIMARY } from '../../../../constants';
+import { VIEW_NAV_GET_STARTED } from '../../../../constants';
 
 const StepGetStarted = () => {
 	const location = useLocation();
