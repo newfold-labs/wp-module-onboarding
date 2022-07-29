04cc97777b853a5f3ff01e97b90c3f3276b7a230
Allen Benny <allen.officially@gmail.com>
2022-07-29 15:56:20 +0530

    Added Translation for Skip This Step in Basic Info

--- src/OnboardingSPA/pages/Steps/BasicInfo/basicInfoForm.js
+++ src/OnboardingSPA/pages/Steps/BasicInfo/basicInfoForm.js
@@ -121,7 +121,7 @@ const BasicInfoForm = () => {
                     <MiniPreview icon={siteLogo} title={siteTitle} desc={siteDesc} />
                 </div>
             </div>
-            <a classname="skip-button-basic-info" onClick={(e) => skipThisStep()} >Skip this step</a>
+            <a classname="skip-button-basic-info" onClick={(e) => skipThisStep()} >{__('Skip this Step', 'wp-module-onboarding')}</a>
         </div>
     );
 };
