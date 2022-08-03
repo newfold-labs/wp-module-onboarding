a989c0f4334851b296b563362b86084714a185db
aulisius <aulisius7@gmail.com>
2022-08-03 14:54:01 +0530

    Revert formatting changes

--- src/OnboardingSPA/data/routes/ecommerce-flow.js
+++ src/OnboardingSPA/data/routes/ecommerce-flow.js
@@ -9,12 +9,12 @@ import {
 	initialTopSteps as defaultInitialTopSteps,
 } from './default-flow';
 
-const StepAddress = lazy(() =>
-	import('../../pages/Steps/Ecommerce/StepAddress')
+const StepAddress = lazy( () =>
+	import( '../../pages/Steps/Ecommerce/StepAddress' )
 );
-const StepTax = lazy(() => import('../../pages/Steps/Ecommerce/StepTax'));
-const StepProducts = lazy(() =>
-	import('../../pages/Steps/Ecommerce/StepProducts')
+const StepTax = lazy( () => import( '../../pages/Steps/Ecommerce/StepTax' ) );
+const StepProducts = lazy( () =>
+	import( '../../pages/Steps/Ecommerce/StepProducts' )
 );
 
 import { VIEW_NAV_ECOMMERCE_STORE_INFO } from '../../../constants';
@@ -22,13 +22,16 @@ import { VIEW_NAV_ECOMMERCE_STORE_INFO } from '../../../constants';
 export const ecommerceSteps = [
 	{
 		path: '/ecommerce/step/address',
-		title: __('Street Address', 'wp-module-onboarding'),
-		heading: __('Street Address', 'wp-module-onboarding'),
+		title: __( 'Street Address', 'wp-module-onboarding' ),
+		heading: __( 'Street Address', 'wp-module-onboarding' ),
 		subheading: __(
 			'In this step you confirm the business address of your store. Simply confirm the one you provided during your initial  Bluehost account setup or provide a new one.',
 			'wp-module-onboarding'
 		),
-		description: __('wp-module-onboarding'),
+		description: __(
+			'In this step you confirm the business address of your store. Simply confirm the one you provided during your initial  Bluehost account setup or provide a new one.',
+			'wp-module-onboarding'
+		),
 		Component: StepAddress,
 		Icon: store,
 		priority: 41,
@@ -36,14 +39,14 @@ export const ecommerceSteps = [
 	},
 	{
 		path: '/ecommerce/step/tax',
-		title: __('Tax Info', 'wp-module-onboarding'),
-		heading: __('Tax Info', 'wp-module-onboarding'),
+		title: __( 'Tax Info', 'wp-module-onboarding' ),
+		heading: __( 'Tax Info', 'wp-module-onboarding' ),
 		subheading: __(
 			'By using your store address we can automatically calculate how much sales tax should be collected by your customers. Taxes can be configure at anytime in the WooCommerce Settings tab.',
 			'wp-module-onboarding'
 		),
 		description: __(
-			'In this section, you can provide more information about your products and business, which will help us tailor your store setup experience and identify possible extensions you’ll need for your online store.',
+			'In this section, you can provide more information about your products and business, which will help us tailor your store setup experience and identify possible extensions you\'ll need for your online store.',
 			'wp-module-onboarding'
 		),
 		Component: StepTax,
@@ -53,14 +56,14 @@ export const ecommerceSteps = [
 	},
 	{
 		path: '/ecommerce/step/products',
-		title: __('Product Info', 'wp-module-onboarding'),
-		heading: __('Product Info', 'wp-module-onboarding'),
+		title: __( 'Product Info', 'wp-module-onboarding' ),
+		heading: __( 'Product Info', 'wp-module-onboarding' ),
 		subheading: __(
 			'Hello, add a subheading for the learn more sidebar.',
 			'wp-module-onboarding'
 		),
 		description: __(
-			'Hello, add a description for the learn more sidebar.',
+			'In this section, you can provide more information about your products and business, which will help us tailor your store setup experience and identify possible extensions you\'ll need for your online store.',
 			'wp-module-onboarding'
 		),
 		Component: StepProducts,
@@ -74,41 +77,45 @@ export const steps = orderBy(
 	[
 		...filter(
 			defaultInitialSteps,
-			(step) => !step.path.includes('/step/top-priority')
+			(step) => ! step.path.includes( '/step/top-priority' )
 		),
 		...ecommerceSteps,
 	],
-	['priority'],
-	['asc']
+	[ 'priority' ],
+	[ 'asc' ]
 );
 
 export const routes = orderBy(
-	[...steps, ...defaultInitialRoutes],
-	['priority'],
-	['asc']
+	[ ...steps, ...defaultInitialRoutes ],
+	[ 'priority' ],
+	[ 'asc' ]
 );
 
 export const initialTopSteps = () => {
-	const topSteps = filter(ecommerceSteps, (step) => {
-		return !step.path.includes('/ecommerce/step');
+	const topSteps = filter( ecommerceSteps, ( step ) => {
+		return ! step.path.includes( '/ecommerce/step' );
 	});
 
 	const ecommerceStep = {
 		/* This is a pseudo step to stand-in for all StoreInfo steps and does not have a Component to render */
 		path: '/ecommerce/step/address',
-		title: __('Store Info', 'wp-module-onboarding'),
+		title: __( 'Store Info', 'wp-module-onboarding' ),
 		description: '',
 		Icon: store,
 		VIEW: VIEW_NAV_ECOMMERCE_STORE_INFO,
 		priority: 41 /* matches priority for first store info step */,
 	};
 
-	topSteps.push(ecommerceStep);
+	topSteps.push( ecommerceStep );
 
 	const filteredSteps = filter(
 		defaultInitialTopSteps(),
-		(step) => !step.path.includes('/step/top-priority')
+		( step ) => ! step.path.includes( '/step/top-priority' )
 	);
 
-	return orderBy([...filteredSteps, ...topSteps], ['priority'], ['asc']);
+	return orderBy(
+		[ ...filteredSteps, ...topSteps ],
+		[ 'priority' ],
+		[ 'asc' ]
+	);
 };
