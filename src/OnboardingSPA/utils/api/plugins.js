787f60dfdbe237d0c7e5aed2353a55ec42561563
unknown <devarunshenoy99@gmail.com>
2022-08-04 01:35:55 +0530

    PRESS2-51 use constant from commonapi for plugin init call

--- src/OnboardingSPA/utils/api/plugins.js
+++ src/OnboardingSPA/utils/api/plugins.js
@@ -1,10 +1,10 @@
-import { apiBase } from '../../../constants';
+import { baseAPI } from './commonAPI';
 
 import apiFetch from '@wordpress/api-fetch';
 
 export const init = () => {
 	apiFetch( {
-		url: `${ window.location.protocol }//${ window.location.host }/index.php${ apiBase }plugins/initialize&flow=${window.nfdOnboarding.currentFlow}`,
+		url: `${ baseAPI }plugins/initialize&flow=${window.nfdOnboarding.currentFlow}`,
 		method: 'POST',
 		headers: {
 			'X-NFD-ONBOARDING': window.nfdOnboarding.pluginInstallHash,
