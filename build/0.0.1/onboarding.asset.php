bce86314708db109848038e28199a1a45814ddfd
David Ryan <dave@bluehost.com>
2022-08-17 14:19:11 -0700

    regenerate dependencies for 0.0.3

--- build/0.0.1/onboarding.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-a11y', 'wp-api-fetch', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-data-controls', 'wp-deprecated', 'wp-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-preferences', 'wp-primitives', 'wp-url'), 'version' => '0db6d4b9acbcc737e4acf81e662bc8e4');
