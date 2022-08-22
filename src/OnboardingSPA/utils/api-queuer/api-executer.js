33d8dfc25a25a0ef872e80ee93bc62d0f2a0f246
Allen Benny <allen.officially@gmail.com>
2022-08-22 08:40:04 +0530

    added API requests to the api-executer function

--- src/OnboardingSPA/utils/api-queuer/api-executer.js
+++ src/OnboardingSPA/utils/api-queuer/api-executer.js
@@ -1,8 +1,18 @@
+import { setFlow } from '../api/flow';
+import { setSettings } from '../api/settings';
 import { FLOW_SYNC, SETTINGS_SYNC } from './constants';
 
 // This Function is responsible to execute requests in a sequence
 export async function apiExecuter(storeData, requests) {
 
-    // Get all the Requests in the Queue 
-    console.log(requests);
+    requests.forEach(request => {
+        switch (request) {
+            case FLOW_SYNC: setFlow(storeData);
+                break;
+            case SETTINGS_SYNC: setSettings(storeData?.data?.socialData);
+                break;
+            default:
+                break;
+        }
+    });
 }
