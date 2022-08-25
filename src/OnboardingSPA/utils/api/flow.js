7c0ba8f3d14ba8f0baf3ddd52fcb83641e3fe625
arunshenoy99 <devarunshenoy99@gmail.com>
2022-08-25 12:22:27 +0530

    PRESS2-168 use common function for flow data

--- src/OnboardingSPA/utils/api/flow.js
+++ src/OnboardingSPA/utils/api/flow.js
@@ -1,18 +1,19 @@
 import { resolve } from './resolve';
 import { onboardingRestBase } from '../../../constants';
+import { onboardingRestURL } from './common';
 
 import apiFetch from '@wordpress/api-fetch';
 
 export async function getFlow() {
 	return await resolve(
-		apiFetch( { url: `${ onboardingRestBase }/flow&flow=${ window.nfdOnboarding.currentFlow }` } ).then()
+		apiFetch( { url: onboardingRestURL( 'flow' ) } ).then()
 	);
 }
 
 export async function setFlow( data ) {
 	return await resolve(
 		apiFetch( {
-			url: `${ onboardingRestBase }/flow&flow=${ window.nfdOnboarding.currentFlow }`,
+			url: onboardingRestURL( 'flow' ),
 			method: 'POST',
 			data,
 		} ).then()
