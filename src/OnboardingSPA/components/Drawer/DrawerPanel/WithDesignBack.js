a92c9b29cf1fadc713dd90f79345f501225333c0
arunshenoy99 <devarunshenoy99@gmail.com>
2022-09-19 02:36:32 +0530

    fix design back step

--- src/OnboardingSPA/components/Drawer/DrawerPanel/WithDesignBack.js
+++ src/OnboardingSPA/components/Drawer/DrawerPanel/WithDesignBack.js
@@ -1,8 +1,14 @@
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { chevronLeft } from '@wordpress/icons';
+import { useDispatch } from '@wordpress/data';
+
+import { store as nfdOnboardingStore } from '../../../store';
+import { VIEW_NAV_DESIGN } from '../../../../constants';
 
 const WithDesignBack = ( { children } ) => {
+	const { setDrawerActiveView } = useDispatch( nfdOnboardingStore );
+
 	return (
 		<div className="is-drawer-fade">
 			<Button
