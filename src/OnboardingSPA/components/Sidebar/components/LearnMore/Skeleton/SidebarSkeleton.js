4dfc5e4f00d99bf85d7aba8180dd69d3024647d2
Allen Benny <allen.officially@gmail.com>
2022-09-28 11:48:50 +0530

    Designed The Skeleton and added animation

--- src/OnboardingSPA/components/Sidebar/components/LearnMore/Skeleton/SidebarSkeleton.js
+++ src/OnboardingSPA/components/Sidebar/components/LearnMore/Skeleton/SidebarSkeleton.js
@@ -5,27 +5,31 @@ const SidebarSkeleton = () => {
         <div className = 'sidebar-skeleton'>
             <div className='sidebar-skeleton-header'>
                 <div className='sidebar-skeleton-header-top'>
-                    <div className='sidebar-skeleton-header-top-profile' />
-                    <div className='sidebar-skeleton-header-top-header' />
+                    <div className='shimmer sidebar-skeleton-header-top-profile' />
+                    <div className='shimmer sidebar-skeleton-header-top-header' />
                 </div>
                 <div className='sidebar-skeleton-header-below'>
-                    <div className='sidebar-skeleton-header-below-subheading-1' />
-                    <div className='sidebar-skeleton-header-below-subheading-2' />
+                    <div className='shimmer sidebar-skeleton-header-below-subheading-1' />
+                    <div className='shimmer sidebar-skeleton-header-below-subheading-2' />
                 </div>
             </div>
+            <div className='sidebar-skeleton-divider'/>
             <div className='sidebar-skeleton-body'>
-                <div className='sidebar-skeleton-body-image'/>
+                <div className='shimmer sidebar-skeleton-body-image'/>
             </div>
+            <div className='sidebar-skeleton-divider' />
             <div className='sidebar-skeleton-footer'>
-                <div className='sidebar-skeleton-footer-line-1' />
-                <div className='sidebar-skeleton-footer-line-2' />
-                <div className='sidebar-skeleton-footer-line-3' />
-                <div className='sidebar-skeleton-footer-line-4' />
-                <div className='sidebar-skeleton-footer-line-5' />
+                <div className='shimmer sidebar-skeleton-footer-line-1' />
+                <div className='shimmer sidebar-skeleton-footer-line-2' />
+                <div className='shimmer sidebar-skeleton-footer-line-3' />
+                <div className='shimmer sidebar-skeleton-footer-line-4' />
+                <div className='shimmer sidebar-skeleton-footer-line-5' />
 
                 <div className='sidebar-skeleton-footer-buttons'>
-                    <div className='sidebar-skeleton-footer-buttons-button-1' />
-                    <div className='sidebar-skeleton-footer-buttons-button-2' />
+                    <div className='shimmer sidebar-skeleton-footer-buttons-button-1'>
+                        <div className='shimmer-1 sidebar-skeleton-footer-buttons-button-2' />
+                    </div>
+                    <div className='shimmer-1 sidebar-skeleton-footer-buttons-button-3' />
                 </div>
             </div>
         </div>
