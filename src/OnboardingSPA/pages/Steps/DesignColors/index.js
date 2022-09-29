c90546cca5e2d724c1f05b8f217862637f503de2
Allen Benny <allen.officially@gmail.com>
2022-09-29 10:34:01 +0530

    Added a Live Preview for the Colors Step

--- src/OnboardingSPA/pages/Steps/DesignColors/index.js
+++ src/OnboardingSPA/pages/Steps/DesignColors/index.js
@@ -1,19 +1,101 @@
-import { useDispatch } from '@wordpress/data';
-import { useEffect } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { useLocation } from 'react-router-dom';
+import { useViewportMatch } from '@wordpress/compose';
+import { useState, useEffect } from '@wordpress/element';
+import { useSelect, useDispatch } from '@wordpress/data';
 
+import { getPatterns } from '../../../utils/api/patterns';
+import { getGlobalStyles } from '../../../utils/api/themes';
+import { store as nfdOnboardingStore } from '../../../store';
+import { LivePreview } from '../../../components/LivePreview';
 import CommonLayout from '../../../components/Layouts/Common';
 import { VIEW_DESIGN_COLORS } from '../../../../constants';
-import { store as nfdOnboardingStore } from '../../../store';
+import { useGlobalStylesOutput } from '../../../utils/global-styles/use-global-styles-output';
 
 const StepDesignColors = () => {
-	const { setDrawerActiveView, setIsDrawerOpened, setIsSidebarOpened } =
-		useDispatch( nfdOnboardingStore );
-	useEffect( () => {
-		setIsSidebarOpened( false );
-		setIsDrawerOpened( true );
-		setDrawerActiveView( VIEW_DESIGN_COLORS );
-	}, [] );
-	return <CommonLayout></CommonLayout>;
+	const location = useLocation();
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [pattern, setPattern] = useState();
+
+	const isLargeViewport = useViewportMatch('medium');
+	const {
+		currentStep,
+		currentData,
+		storedPreviewSettings,
+	} = useSelect((select) => {
+		return {
+			currentStep: select(nfdOnboardingStore).getStepFromPath(
+				location.pathname
+			),
+			currentData:
+				select(nfdOnboardingStore).getCurrentOnboardingData(),
+			storedPreviewSettings:
+				select(nfdOnboardingStore).getPreviewSettings(),
+		};
+	}, []);
+
+	const {
+		setDrawerActiveView,
+		setIsDrawerOpened,
+		setIsSidebarOpened,
+		setIsDrawerSuppressed,
+		updatePreviewSettings,
+		setCurrentOnboardingData,
+	} = useDispatch(nfdOnboardingStore);
+
+	useEffect(() => {
+		if (isLargeViewport) {
+			setIsDrawerOpened(true);
+		}
+		setIsSidebarOpened(false);
+		setIsDrawerSuppressed(false);
+		setDrawerActiveView(VIEW_DESIGN_COLORS);
+	}, []);
+
+	const getStylesAndPatterns = async () => {
+		const pattern = await getPatterns(currentStep.patternId, true);
+		const globalStyles = await getGlobalStyles();
+		let selectedGlobalStyle;
+		if (currentData.data.theme.variation) {
+			selectedGlobalStyle = globalStyles.body.filter(
+				(globalStyle) =>
+					globalStyle.title === currentData.data.theme.variation
+			)[0];
+		} else {
+			selectedGlobalStyle = globalStyles.body[0];
+		}
+		updatePreviewSettings(
+			useGlobalStylesOutput(selectedGlobalStyle, storedPreviewSettings)
+		);
+		setPattern(pattern?.body);
+		setIsLoaded(true);
+	};
+
+	
+	useEffect(() => {
+		if (!isLoaded) getStylesAndPatterns();
+	}, [isLoaded]);
+
+	return (
+		<CommonLayout className="theme-colors-preview">
+			<div className="theme-colors-preview__title-bar">
+				<div className="theme-colors-preview__title-bar__browser">
+					<span className="theme-colors-preview__title-bar__browser__dot"></span>
+					<span className="theme-colors-preview__title-bar__browser__dot"></span>
+					<span className="theme-colors-preview__title-bar__browser__dot"></span>
+				</div>
+			</div>
+			<div className="theme-colors-preview__live-preview-container">
+				{pattern && (
+					<LivePreview
+						blockGrammer={pattern}
+						styling={'custom'}
+						viewportWidth={1300}
+					/>
+				)}
+			</div>
+		</CommonLayout>
+	);
 };
 
 export default StepDesignColors;
