b2f16dfb58543e9b32db7a95108d7814f242f7ba
Yashita <yashita.jain@newfold.com>
2022-10-07 19:06:24 +0530

    bump onboarding v0.2.3 to 0.2.4

---
+++ build/0.2.4/onboarding.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-a11y', 'wp-api-fetch', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-data-controls', 'wp-deprecated', 'wp-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-preferences', 'wp-primitives', 'wp-url'), 'version' => '34ef1ca04f60898fd871e280ca20b918');
