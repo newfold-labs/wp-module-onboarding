022bb7879e3771b1dbd6103e138fffed4209f1ea
arunshenoy99 <devarunshenoy99@gmail.com>
2022-11-22 13:56:06 +0530

    use GlobalStylesProvider for global styles

--- src/OnboardingSPA/pages/Steps/SitePages/index.js
+++ src/OnboardingSPA/pages/Steps/SitePages/index.js
@@ -6,16 +6,17 @@ import { useViewportMatch } from '@wordpress/compose';
 import { store as nfdOnboardingStore } from '../../../store';
 import CommonLayout from '../../../components/Layouts/Common';
 import HeadingWithSubHeading from '../../../components/HeadingWithSubHeading';
-import { useGlobalStylesOutput } from '../../../utils/global-styles/use-global-styles-output';
 import { getPatterns } from '../../../utils/api/patterns';
-import { getGlobalStyles } from '../../../utils/api/themes';
 import {
 	VIEW_NAV_PRIMARY,
 	THEME_STATUS_ACTIVE,
 	THEME_STATUS_NOT_ACTIVE,
 } from '../../../../constants';
 import { DesignStateHandler } from '../../../components/StateHandlers';
-import { LivePreviewSelectableCardWithInfo } from '../../../components/LivePreview';
+import {
+	GlobalStylesProvider,
+	LivePreviewSelectableCardWithInfo,
+} from '../../../components/LivePreview';
 
 const StepSitePages = () => {
 	const isLargeViewport = useViewportMatch( 'medium' );
@@ -27,26 +28,22 @@ const StepSitePages = () => {
 	const [ sitePages, setSitePages ] = useState();
 	const [ checkedPages, setCheckedPages ] = useState( [] );
 
-	const { currentStep, currentData, themeStatus, storedPreviewSettings } =
-		useSelect( ( select ) => {
-			return {
-				storedPreviewSettings:
-					select( nfdOnboardingStore ).getPreviewSettings(),
-				currentStep: select( nfdOnboardingStore ).getStepFromPath(
-					location.pathname
-				),
-				currentData:
-					select( nfdOnboardingStore ).getCurrentOnboardingData(),
-				themeStatus: select( nfdOnboardingStore ).getThemeStatus(),
-			};
-		}, [] );
+	const { currentStep, currentData, themeStatus } = useSelect( ( select ) => {
+		return {
+			currentStep: select( nfdOnboardingStore ).getStepFromPath(
+				location.pathname
+			),
+			currentData:
+				select( nfdOnboardingStore ).getCurrentOnboardingData(),
+			themeStatus: select( nfdOnboardingStore ).getThemeStatus(),
+		};
+	}, [] );
 
 	const {
 		setDrawerActiveView,
 		setIsDrawerOpened,
 		setIsSidebarOpened,
 		updateThemeStatus,
-		updatePreviewSettings,
 		setCurrentOnboardingData,
 	} = useDispatch( nfdOnboardingStore );
 
@@ -81,22 +78,6 @@ const StepSitePages = () => {
 				setCheckedPages( checkedPages );
 			}
 		}
-		const globalStylesResponse = await getGlobalStyles();
-		if ( globalStylesResponse?.error ) {
-			return updateThemeStatus( THEME_STATUS_NOT_ACTIVE );
-		}
-		let selectedGlobalStyle;
-		if ( currentData.data.theme.variation ) {
-			selectedGlobalStyle = globalStylesResponse.body.filter(
-				( globalStyle ) =>
-					globalStyle.title === currentData.data.theme.variation
-			)[ 0 ];
-		} else {
-			selectedGlobalStyle = globalStylesResponse.body[ 0 ];
-		}
-		updatePreviewSettings(
-			useGlobalStylesOutput( selectedGlobalStyle, storedPreviewSettings )
-		);
 		setIsLoaded( true );
 	};
 
@@ -147,25 +128,30 @@ const StepSitePages = () => {
 
 	return (
 		<DesignStateHandler>
-			<CommonLayout>
-				<div className="site-pages">
-					<HeadingWithSubHeading
-						title={ currentStep?.heading }
-						subtitle={ currentStep?.subheading }
-					/>
-					<div className="site-pages__list">
-						{ sitePages &&
-							buildPreviews().slice( 0, MAX_PREVIEWS_PER_ROW ) }
+			<GlobalStylesProvider>
+				<CommonLayout>
+					<div className="site-pages">
+						<HeadingWithSubHeading
+							title={ currentStep?.heading }
+							subtitle={ currentStep?.subheading }
+						/>
+						<div className="site-pages__list">
+							{ sitePages &&
+								buildPreviews().slice(
+									0,
+									MAX_PREVIEWS_PER_ROW
+								) }
+						</div>
+						<div className="site-pages__list">
+							{ sitePages &&
+								buildPreviews().slice(
+									MAX_PREVIEWS_PER_ROW,
+									sitePages.length
+								) }
+						</div>
 					</div>
-					<div className="site-pages__list">
-						{ sitePages &&
-							buildPreviews().slice(
-								MAX_PREVIEWS_PER_ROW,
-								sitePages.length
-							) }
-					</div>
-				</div>
-			</CommonLayout>
+				</CommonLayout>
+			</GlobalStylesProvider>
 		</DesignStateHandler>
 	);
 };
