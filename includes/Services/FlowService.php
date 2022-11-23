057e0d2b3d45d2ffbea3f04c51148b57b850e640
Yashita <yashita.jain@newfold.com>
2022-11-23 21:00:26 +0530

    Update FlowService.php

--- includes/Services/FlowService.php
+++ includes/Services/FlowService.php
@@ -12,8 +12,8 @@ class FlowService {
         if ( ! ( $flow_data = self::read_flow_data_from_wp_option() ) )
             $flow_data = self::get_default_flow_data();
 		if ($flow_data_fixes) {
-			foreach($flow_data_fixes as $key=>$value) {
-				$flow_data = self::rename_keys($value, Flows::get_data(), $flow_data);
+			foreach($flow_data_fixes as $key=>$fixes_data) {
+				$flow_data = self::rename_keys($fixes_data, Flows::get_data(), $flow_data);
 				self::update_wp_options_data_in_database($flow_data);
 			}
 		}
@@ -39,10 +39,8 @@ class FlowService {
 	 * function to compare and add Flows::get_data() items to database
 	 */
 	private static function update_flow_data($flow_data) {
-		if ( array_diff(self::flatten_array(Flows::get_data()), self::flatten_array($flow_data)) ) {
-			$flow_data = array_replace_recursive( Flows::get_data(), $flow_data);
-			self::update_wp_options_data_in_database( $flow_data );
-		}
+		$flow_data = array_replace_recursive( Flows::get_data(), $flow_data);
+		self::update_wp_options_data_in_database( $flow_data );
 		$delete_flow_data = self::delete_wp_options_data_in_database(Flows::get_data(), $flow_data);
 		if ( ! is_null($delete_flow_data) )
 			$flow_data = $delete_flow_data;
@@ -62,25 +60,18 @@ class FlowService {
 		return $flow_data;
 	}
 
-	public static function rename_keys($flow_data_fixes, &$data, &$input){
-		foreach ($input as $key => $value) {
+	public static function rename_keys($flow_data_fixes, &$default_flow_data, &$flow_data){
+		foreach ($flow_data as $key => $value) {
 			if (strcmp($key, $flow_data_fixes['old_key']) == 0) {
-				if (array_key_exists('new_value', $flow_data_fixes)) {
-					if(! array_key_exists('new_key', $flow_data_fixes)) 
-						$input[$key] = $flow_data_fixes['new_value'];
-					else {
-						$key = $flow_data_fixes['new_key'];	
-						unset($input[$flow_data_fixes['old_key']]);
-						$input[$key] = $flow_data_fixes['new_value'];
-					}
-				}
 				$key = $flow_data_fixes['new_key'];
-				unset($input[$flow_data_fixes['old_key']]);
+				unset($flow_data[$flow_data_fixes['old_key']]);
+				if (array_key_exists('new_value', $flow_data_fixes))
+					$flow_data[$key] = $flow_data_fixes['new_value'];
 			}
 			if (is_array($value)) 
-				$input[$key] = self::rename_keys( $flow_data_fixes, $data[$key], $value);
+				$flow_data[$key] = self::rename_keys( $flow_data_fixes, $default_flow_data[$key], $value);
 		}
-		return $input; 
+		return $flow_data; 
 	}
 
     /*
@@ -118,17 +109,6 @@ class FlowService {
 		return $data;
 	}
 
-    private static function flatten_array( $array ) {
-		$all_keys = array();
-		foreach ($array as $key => $value) {
-			$all_keys[] = $key;
-			if (is_array($value)) {
-				$all_keys = array_merge($all_keys, self::flatten_array($value));
-			}
-		}
-		return $all_keys;
-	}
-
     /*
 	 * function to search for key in array recursively with case sensitive exact match
 	 */
@@ -140,12 +120,12 @@ class FlowService {
 				if (count(array_filter(array_keys($params), 'is_string')) === 0) {
 					if ( is_array($value) && !empty($value) ) 
 						self::check_key_in_nested_array($value, $flow_data[$key], $key);
-					$mismatch_key[] = $header_key. " => " . $value;
+					else $mismatch_key[] = $header_key. " => " . $value;
 				}
 				else $mismatch_key[]  = $header_key. " => " . $key;
 			}
 			elseif ( is_array( $value ) && !empty($value) ) {
-				self::check_key_in_nested_array($value, $flow_data, $key);
+				self::check_key_in_nested_array($value, $flow_data[$key], $key);
 			}
 		}
 		return implode(", ", $mismatch_key);
