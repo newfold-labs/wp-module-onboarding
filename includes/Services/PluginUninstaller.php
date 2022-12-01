066cf9e4b75e3ad1062c7c232bf237a4fd210f8f
Allen Benny <allen.officially@gmail.com>
2022-12-01 11:29:59 +0530

    Refactored Code

--- includes/Services/PluginUninstaller.php
+++ includes/Services/PluginUninstaller.php
@@ -3,35 +3,28 @@ namespace NewfoldLabs\WP\Module\Onboarding\Services;
 
 use NewfoldLabs\WP\Module\Onboarding\Data\Plugins;
 
+/**
+ * Class PluginUninstaller
+ * This class is responsible to Uninstall a specified plugin
+ */
+
 class PluginUninstaller {
 
 	public static function uninstall( $plugin ) {
 
 		$plugin_list = Plugins::get_squashed();
+		// Gets the specified path for the Plugin from the predefined list
 		$plugin_path = $plugin_list[ $plugin ]['path'];
 
 		if ( isset( $plugin_path ) && self::is_plugin_installed( $plugin_path ) ) {
 
-			if ( \is_plugin_active( $plugin_path ) ) {
-				\deactivate_plugins( $plugin_path );
-			}
+			self::deactivate_plugin_if_active( $plugin_path );
+			$res = self::delete_plugin( $plugin_path );
 
-			if ( ! self::connect_to_filesystem() ) {
-				return new \WP_Error(
-					'nfd_onboarding_error',
-					'Could not connect to the filesystem.',
-					array( 'status' => 500 )
-				);
+			// If Deletion is not successful throw an error for a retry.
+			if ( is_wp_error( $res ) ) {
+				return $res;
 			}
-
-			if ( ! \delete_plugins( array( $plugin_path ) ) ) {
-				return new \WP_Error(
-					'nfd_onboarding_error',
-					'Unable to Delete the Plugin',
-					array( 'status' => 500 )
-				);
-			}
-
 		}
 
 		return new \WP_REST_Response(
@@ -48,7 +41,10 @@ class PluginUninstaller {
 	 * @return boolean
 	 */
 	public static function exists( $plugin ) {
-		 $plugin_path = $plugin . '/' . $plugin . '.php';
+
+		$plugin_list = Plugins::get_squashed();
+		$plugin_path = $plugin_list[ $plugin ]['path'];
+
 		if ( ! self::is_plugin_installed( $plugin_path ) ) {
 			 return false;
 		}
@@ -75,6 +71,26 @@ class PluginUninstaller {
 	}
 
 	/**
+	 * Deactivates a Plugin if it is activated.
+	 *
+	 * @param string $plugin_path Path to the plugin's header file.
+	 */
+	public static function deactivate_plugin_if_active( $plugin_path ) {
+
+		// Checks if the necessary functions exist
+		if ( ! function_exists( 'is_plugin_active' ) || ! function_exists( 'deactivate_plugins' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		}
+
+		// Determines whether a plugin is active.
+		if ( \is_plugin_active( $plugin_path ) ) {
+			// Deactivates a single plugin or multiple plugins.
+			// /wp-admin/includes/plugin.php
+			\deactivate_plugins( $plugin_path );
+		}
+	}
+
+	/**
 	 * Establishes a connection to the wp_filesystem.
 	 *
 	 * @return boolean
@@ -92,6 +108,39 @@ class PluginUninstaller {
 
 		if ( ! \WP_Filesystem( $creds ) ) {
 			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Deletes a Plugin if it exists.
+	 *
+	 * @param string $plugin_path Path to the plugin's header file.
+	 * @return boolean|\WP_Error
+	 */
+	public static function delete_plugin( $plugin_path ) {
+
+		// Checks if the necessary functions exist
+		if ( ! function_exists( 'delete_plugins' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		}
+
+		if ( ! self::connect_to_filesystem() ) {
+			return new \WP_Error(
+				'nfd_onboarding_error',
+				'Could not connect to the filesystem.',
+				array( 'status' => 500 )
+			);
+		}
+
+		// Removes directory and files of a plugin
+		if ( ! \delete_plugins( array( $plugin_path ) ) ) {
+			return new \WP_Error(
+				'nfd_onboarding_error',
+				'Unable to Delete the Plugin',
+				array( 'status' => 500 )
+			);
 		}
 
 		return true;
