4d996fa5fa66a2058b0332e223fb5c40bcf8c0cc
arunshenoy99 <devarunshenoy99@gmail.com>
2022-12-13 11:03:22 +0530

    remove double useDispatches

--- src/OnboardingSPA/pages/Steps/GetStarted/SiteTypeSetup/SecondarySite/index.js
+++ src/OnboardingSPA/pages/Steps/GetStarted/SiteTypeSetup/SecondarySite/index.js
@@ -1,7 +1,10 @@
 import { __ } from '@wordpress/i18n';
 import CommonLayout from '../../../../../components/Layouts/Common';
 import NewfoldLargeCard from '../../../../../components/NewfoldLargeCard';
-import { SIDEBAR_LEARN_MORE, VIEW_NAV_GET_STARTED } from '../../../../../../constants';
+import {
+	SIDEBAR_LEARN_MORE,
+	VIEW_NAV_GET_STARTED,
+} from '../../../../../../constants';
 import { store as nfdOnboardingStore } from '../../../../../store';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useEffect, useState } from '@wordpress/element';
@@ -11,108 +14,158 @@ import NeedHelpTag from '../../../../../components/NeedHelpTag';
 import content from '../content.json';
 import { translations } from '../../../../../utils/locales/translations';
 
-
 const StepPrimarySetup = () => {
-	const { setDrawerActiveView, setSidebarActiveView, setIsDrawerSuppressed, setIsHeaderNavigationEnabled } = useDispatch(
-		nfdOnboardingStore
-	);
+	const {
+		setDrawerActiveView,
+		setSidebarActiveView,
+		setIsDrawerSuppressed,
+		setIsHeaderNavigationEnabled,
+		setCurrentOnboardingData,
+	} = useDispatch( nfdOnboardingStore );
 
-	useEffect(() => {
+	useEffect( () => {
 		setSidebarActiveView( SIDEBAR_LEARN_MORE );
-		setIsDrawerSuppressed(true);
-		setDrawerActiveView(VIEW_NAV_GET_STARTED);
+		setIsDrawerSuppressed( true );
+		setDrawerActiveView( VIEW_NAV_GET_STARTED );
 		setIsHeaderNavigationEnabled( true );
-	}, []);
+	}, [] );
 
-	const [clickedIndex, changeCategory] = useState(-1);
-	const [inputCategVal, changeInputCateg] = useState('');
+	const [ clickedIndex, changeCategory ] = useState( -1 );
+	const [ inputCategVal, changeInputCateg ] = useState( '' );
 
-
-	const { setCurrentOnboardingData } = useDispatch(nfdOnboardingStore);
-
-	const { currentStep, currentData } = useSelect((select) => {
+	const { currentStep, currentData } = useSelect( ( select ) => {
 		return {
-			currentStep: select(nfdOnboardingStore).getCurrentStep(),
-			currentData: select(nfdOnboardingStore).getCurrentOnboardingData()
+			currentStep: select( nfdOnboardingStore ).getCurrentStep(),
+			currentData:
+				select( nfdOnboardingStore ).getCurrentOnboardingData(),
 		};
-	}, []);
+	}, [] );
 
 	const selectedCategoryInStore = currentData?.data?.siteType?.secondary;
 	const categoriesArray = content.categories;
-	const subCategories = categoriesArray[0]?.subCategories;
+	const subCategories = categoriesArray[ 0 ]?.subCategories;
 
 	/**This condition fills the data in input box if the saved category isn't a subcategory from the content*/
-	if (selectedCategoryInStore && !inputCategVal && subCategories.indexOf(selectedCategoryInStore) === -1) {
-		if (selectedCategoryInStore !== 'secondaryCategory')
-		 changeInputCateg(selectedCategoryInStore);
+	if (
+		selectedCategoryInStore &&
+		! inputCategVal &&
+		subCategories.indexOf( selectedCategoryInStore ) === -1
+	) {
+		if ( selectedCategoryInStore !== 'secondaryCategory' )
+			changeInputCateg( selectedCategoryInStore );
 	}
 
-	/** Function which saves data in redux when category name is put-in via input box */
-	const categoryInput = input => {
-		changeCategory(-1);
-		changeInputCateg(input?.target?.value);
+	/**
+	 * Function which saves data in redux when category name is put-in via input box
+	 *
+	 * @param  input
+	 */
+	const categoryInput = ( input ) => {
+		changeCategory( -1 );
+		changeInputCateg( input?.target?.value );
 		const currentDataCopy = currentData;
-		currentDataCopy.data.siteType['secondary'] = input?.target?.value;
-		setCurrentOnboardingData(currentDataCopy);
-	}
+		currentDataCopy.data.siteType.secondary = input?.target?.value;
+		setCurrentOnboardingData( currentDataCopy );
+	};
 
-	/** Function which saves data in redux when category name is chosen via categories displayed */
-	const handleCategoryClick = (idxOfElm) => {
-		changeCategory(idxOfElm);
-		changeInputCateg('');
+	/**
+	 * Function which saves data in redux when category name is chosen via categories displayed
+	 *
+	 * @param  idxOfElm
+	 */
+	const handleCategoryClick = ( idxOfElm ) => {
+		changeCategory( idxOfElm );
+		changeInputCateg( '' );
 		const currentDataCopy = currentData;
-		currentDataCopy.data.siteType['secondary'] = categoriesArray[0]?.subCategories[idxOfElm];
-		setCurrentOnboardingData(currentDataCopy);
-	}
+		currentDataCopy.data.siteType.secondary =
+			categoriesArray[ 0 ]?.subCategories[ idxOfElm ];
+		setCurrentOnboardingData( currentDataCopy );
+	};
 
 	return (
 		<CommonLayout isBgPrimary isCentered>
 			<NewfoldLargeCard>
 				<div className="nfd-card-heading center">
 					<CardHeader
-						heading={__(currentStep?.heading, 'wp-module-onboarding')}
-						subHeading={sprintf(__(content.subHeading, 'wp-module-onboarding'), translations('SITE'))}
-						question={__(currentStep?.subheading, 'wp-module-onboarding')}
+						heading={ __(
+							currentStep?.heading,
+							'wp-module-onboarding'
+						) }
+						subHeading={ sprintf(
+							__( content.subHeading, 'wp-module-onboarding' ),
+							translations( 'SITE' )
+						) }
+						question={ __(
+							currentStep?.subheading,
+							'wp-module-onboarding'
+						) }
 					/>
 				</div>
 
-				<div className='nfd-setup-secondary-categories'>
-					<div className='nfd-card-category-wrapper'>
+				<div className="nfd-setup-secondary-categories">
+					<div className="nfd-card-category-wrapper">
 						<div className="category-scrolling-wrapper">
-							<span className="icon" style={{ backgroundImage: categoriesArray[0].icon }} />
-							<p className="categName"> {categoriesArray[0].name}</p>
+							<span
+								className="icon"
+								style={ {
+									backgroundImage: categoriesArray[ 0 ].icon,
+								} }
+							/>
+							<p className="categName">
+								{ ' ' }
+								{ categoriesArray[ 0 ].name }
+							</p>
 						</div>
 					</div>
 
-					<div className='subCategoriesSection'>
-						{
-							categoriesArray[0]?.subCategories?.map((item, idx) => {
-								return <span
-									key={item}
-									onClick={(e) => handleCategoryClick(idx)}
-									className={`${(clickedIndex === idx || item === selectedCategoryInStore) ? 'chosenSecondaryCategory ' : ''}nfd-card-category`}>
-									{item}
-								</span>
-							})
-						}
+					<div className="subCategoriesSection">
+						{ categoriesArray[ 0 ]?.subCategories?.map(
+							( item, idx ) => {
+								return (
+									<span
+										key={ item }
+										onClick={ ( e ) =>
+											handleCategoryClick( idx )
+										}
+										className={ `${
+											clickedIndex === idx ||
+											item === selectedCategoryInStore
+												? 'chosenSecondaryCategory '
+												: ''
+										}nfd-card-category` }
+									>
+										{ item }
+									</span>
+								);
+							}
+						) }
 					</div>
 				</div>
 
-				<div className='nfd-setup-primary-second'>
-					<div className='nfd-setup-primary-second-top'>
-						<div className='blackText'>{__(content.tellusHereText, 'wp-module-onboarding')}</div>
+				<div className="nfd-setup-primary-second">
+					<div className="nfd-setup-primary-second-top">
+						<div className="blackText">
+							{ __(
+								content.tellusHereText,
+								'wp-module-onboarding'
+							) }
+						</div>
 						<input
 							type="text"
-							onChange={(e) => categoryInput(e)}
-							className='tellUsInput'
-							placeholder={sprintf(__(content.placeholderSiteTypeInput, 'wp-module-onboarding'), translations('site'))}
-							value={inputCategVal}
+							onChange={ ( e ) => categoryInput( e ) }
+							className="tellUsInput"
+							placeholder={ sprintf(
+								__(
+									content.placeholderSiteTypeInput,
+									'wp-module-onboarding'
+								),
+								translations( 'site' )
+							) }
+							value={ inputCategVal }
 						/>
 					</div>
 				</div>
-				<NavCardButton
-					text={__(content.buttonText)}
-				/>
+				<NavCardButton text={ __( content.buttonText ) } />
 				<NeedHelpTag />
 			</NewfoldLargeCard>
 		</CommonLayout>
@@ -120,4 +173,3 @@ const StepPrimarySetup = () => {
 };
 
 export default StepPrimarySetup;
-
