914f1fba4d75d78538386f9f494e1ee154e39682
Yashita <yashita.jain@newfold.com>
2023-01-05 17:24:40 +0530

    Formatted the file

--- src/OnboardingSPA/components/StateHandlers/StepStateHandler/index.js
+++ src/OnboardingSPA/components/StateHandlers/StepStateHandler/index.js
@@ -3,27 +3,23 @@ import { useState, useEffect } from '@wordpress/element';
 /**
  * A State Handler to manage Steps
  *
- * @param {string} children          The children to be rendered out.
- * @param {number} watch             The variable to be awaited for to be fetched.
+ * @param {string} children The children to be rendered out.
+ * @param {number} watch    The variable to be awaited for to be fetched.
  *
  */
-const StepStateHandler = ({ watch, children }) => {
-    const [rerender, doRerender] = useState(0);
+const StepStateHandler = ( { watch, children } ) => {
+	const [ rerender, doRerender ] = useState( 0 );
 
-    useEffect(() => doRerender(1), [watch]);
+	useEffect( () => doRerender( 1 ), [ watch ] );
 
-    return !watch ? (
-        <div className='step-state-handler--blank'>
-            { children }
-        </div>
-    ) : (
-        <>
-            {<div style={{ display: 'none' }}>{rerender}</div>}
-                <div className='step-state-handler--visible'>
-                {children}
-            </div>
-        </>
-    );
+	return ! watch ? (
+		<div className="step-state-handler--blank">{ children }</div>
+	) : (
+		<>
+			{ <div style={ { display: 'none' } }>{ rerender }</div> }
+			<div className="step-state-handler--visible">{ children }</div>
+		</>
+	);
 };
 
 export default StepStateHandler;
