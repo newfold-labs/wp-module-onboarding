236e463b40e97bb0cdb9ff92fa69a6d29bec5aa0
Yashita <yashita.jain@newfold.com>
2023-01-16 13:14:15 +0530

    Leveraged Animate Component

--- src/OnboardingSPA/pages/Steps/Ecommerce/StepAddress/index.js
+++ src/OnboardingSPA/pages/Steps/Ecommerce/StepAddress/index.js
@@ -13,7 +13,6 @@ import content from '../content.json';
 import countries from '../countries.json';
 import currencies from '../currencies.json';
 import { useWPSettings } from '../useWPSettings';
-import Animate from '../../../../components/Animate';
 import { EcommerceStateHandler } from '../../../../components/StateHandlers';
 
 const StepAddress = () => {
@@ -149,132 +148,131 @@ const StepAddress = () => {
 									'wp-module-onboarding'
 								)}
 							/>
+							{settings === null && <p>Loading your details...</p>}
 						</div>
-						<Animate type="fade-in" after={ settings }>
-							<div className={'store-address-form'}>
-								<div data-name='country'>
+						<div className='store-address-form'>
+							<div data-name='country'>
+								<label aria-required>
+									{__('Where is your store based?', 'wp-module-onboarding')}
+								</label>
+								{settings === null ? (
+									<input type='text' disabled />
+								) : (
+									<select
+										type='text'
+										name='country'
+										required
+										defaultValue={selectedCountry}
+										{...fieldProps}
+									>
+										{countries.map((country) => (
+											<option key={country.code} value={country.code}>
+												{country.name}
+											</option>
+										))}
+									</select>
+								)}
+							</div>
+							<div data-name='woocommerce_store_address'>
+								<label aria-required>
+									{__('Address', 'wp-module-onboarding')}
+								</label>
+								<input
+									name='woocommerce_store_address'
+									type='text'
+									required
+									defaultValue={address?.woocommerce_store_address}
+									{...fieldProps}
+								/>
+							</div>
+							<div
+								data-name='full-address'
+								data-state-empty={states.length === 0}
+							>
+								<div data-name='woocommerce_store_city'>
 									<label aria-required>
-										{__('Where is your store based?', 'wp-module-onboarding')}
+										{__('City', 'wp-module-onboarding')}
 									</label>
-									{settings === null ? (
-										<input type='text' disabled />
-									) : (
+									<input
+										name='woocommerce_store_city'
+										type='text'
+										required
+										defaultValue={address?.woocommerce_store_city}
+										{...fieldProps}
+									/>
+								</div>
+								{states.length === 0 || settings === null ? null : (
+									<div data-name='state'>
+										<label aria-required>
+											{__('State', 'wp-module-onboarding')}
+										</label>
 										<select
 											type='text'
-											name='country'
+											name='state'
 											required
-											defaultValue={selectedCountry}
+											defaultValue={
+												selectedCountry == defaultCountry ? defaultState : ''
+											}
 											{...fieldProps}
 										>
-											{countries.map((country) => (
-												<option key={country.code} value={country.code}>
-													{country.name}
+											<option key={''} value={''} selected />
+											{states.map((state) => (
+												<option key={state.code} value={state.code}>
+													{state.name}
 												</option>
 											))}
 										</select>
-									)}
-								</div>
-								<div data-name='woocommerce_store_address'>
+									</div>
+								)}
+								<div data-name='woocommerce_store_postcode'>
 									<label aria-required>
-										{__('Address', 'wp-module-onboarding')}
+										{__('Postal Code', 'wp-module-onboarding')}
 									</label>
 									<input
-										name='woocommerce_store_address'
+										name='woocommerce_store_postcode'
 										type='text'
 										required
-										defaultValue={address?.woocommerce_store_address}
+										defaultValue={address?.woocommerce_store_postcode}
 										{...fieldProps}
 									/>
 								</div>
-								<div
-									data-name='full-address'
-									data-state-empty={states.length === 0}
-								>
-									<div data-name='woocommerce_store_city'>
-										<label aria-required>
-											{__('City', 'wp-module-onboarding')}
-										</label>
-										<input
-											name='woocommerce_store_city'
-											type='text'
-											required
-											defaultValue={address?.woocommerce_store_city}
-											{...fieldProps}
-										/>
-									</div>
-									{states.length === 0 || settings === null ? null : (
-										<div data-name='state'>
-											<label aria-required>
-												{__('State', 'wp-module-onboarding')}
-											</label>
-											<select
-												type='text'
-												name='state'
-												required
-												defaultValue={
-													selectedCountry == defaultCountry ? defaultState : ''
-												}
-												{...fieldProps}
-											>
-												<option key={''} value={''} selected />
-												{states.map((state) => (
-													<option key={state.code} value={state.code}>
-														{state.name}
-													</option>
-												))}
-											</select>
-										</div>
-									)}
-									<div data-name='woocommerce_store_postcode'>
-										<label aria-required>
-											{__('Postal Code', 'wp-module-onboarding')}
-										</label>
-										<input
-											name='woocommerce_store_postcode'
-											type='text'
-											required
-											defaultValue={address?.woocommerce_store_postcode}
-											{...fieldProps}
-										/>
-									</div>
-								</div>
-								<div>
-									<label aria-required>
-										{__('Email', 'wp-module-onboarding')}
-									</label>
-									<input
-										name='woocommerce_email_from_address'
-										type='email'
-										required
-										defaultValue={address?.woocommerce_email_from_address}
-										{...fieldProps}
-									/>
-								</div>
-								<div>
-									<label>
-										{__(
-											'What currency do you want to display in your store?',
-											'wp-module-onboarding'
-										)}
-									</label>
-									<select
-										type='text'
-										name='woocommerce_currency'
-										value={address?.woocommerce_currency}
-										{...fieldProps}
-									>
-										{Object.entries(currencies).map(([code, currency]) => (
-											<option
-												key={code}
-												value={code}
-												dangerouslySetInnerHTML={{ __html: currency }}
-											/>
-										))}
-									</select>
-								</div>
-								<em style={{ display: 'inline' }}>* required</em>
 							</div>
-						</Animate>
+							<div>
+								<label aria-required>
+									{__('Email', 'wp-module-onboarding')}
+								</label>
+								<input
+									name='woocommerce_email_from_address'
+									type='email'
+									required
+									defaultValue={address?.woocommerce_email_from_address}
+									{...fieldProps}
+								/>
+							</div>
+							<div>
+								<label>
+									{__(
+										'What currency do you want to display in your store?',
+										'wp-module-onboarding'
+									)}
+								</label>
+								<select
+									type='text'
+									name='woocommerce_currency'
+									value={address?.woocommerce_currency}
+									{...fieldProps}
+								>
+									{Object.entries(currencies).map(([code, currency]) => (
+										<option
+											key={code}
+											value={code}
+											dangerouslySetInnerHTML={{ __html: currency }}
+										/>
+									))}
+								</select>
+							</div>
+							<em style={{ display: 'inline' }}>* required</em>
+						</div>
 						<button
 							className='nfd-nav-card-button nfd-card-button'
 							disabled={settings === null}
