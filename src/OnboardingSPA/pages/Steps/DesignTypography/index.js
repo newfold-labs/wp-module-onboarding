0adfcb6286cfb3e77b233cb3b053c7b57c02d019
Allen Benny <allen.officially@gmail.com>
2023-01-28 10:16:07 +0530

    Added themeStatus to useEffect like other steps

--- src/OnboardingSPA/pages/Steps/DesignTypography/index.js
+++ src/OnboardingSPA/pages/Steps/DesignTypography/index.js
@@ -54,7 +54,7 @@ const StepDesignTypography = () => {
 	useEffect( () => {
 		if ( ! isLoaded && THEME_STATUS_ACTIVE === themeStatus )
 			getFontPatterns();
-	}, [ isLoaded ] );
+	}, [ isLoaded, themeStatus ] );
 
 	return (
 		<DesignStateHandler>
