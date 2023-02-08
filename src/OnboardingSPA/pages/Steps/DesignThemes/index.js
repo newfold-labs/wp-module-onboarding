d365ace57de590da5b7866c8ce323ec964eb8b2d
Allen Benny <allen.officially@gmail.com>
2023-02-08 15:29:30 +0530

    Update index.js

--- src/OnboardingSPA/pages/Steps/DesignThemes/index.js
+++ src/OnboardingSPA/pages/Steps/DesignThemes/index.js
@@ -13,7 +13,7 @@ const StepDesignThemes = () => {
 		setIsHeaderNavigationEnabled,
 	} = useDispatch( nfdOnboardingStore );
 
-	useEffect( () => { setSidebarActiveView(SIDEBAR_LEARN_MORE);
+	useEffect( () => {  setSidebarActiveView(SIDEBAR_LEARN_MORE);
 		setIsDrawerOpened(true);
 		setDrawerActiveView(VIEW_DESIGN_THEMES);
 		setIsHeaderNavigationEnabled( true );
