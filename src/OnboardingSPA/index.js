6aaae687923bcc6d50f608dc3a19509984a11d76
arunshenoy99 <devarunshenoy99@gmail.com>
2023-02-22 12:19:06 +0530

    fix workflow

--- src/OnboardingSPA/index.js
+++ src/OnboardingSPA/index.js
@@ -38,6 +38,7 @@ const initializeFlowData = ( currentData ) => {
 export async function initializeNFDOnboarding( id, runtime ) {
 	initializePlugins();
 
+
 	initializeThemes();
 	setInterval( cronTrigger, 45000 );
 
