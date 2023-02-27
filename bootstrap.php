f102a2c5818e48c33560905fa786f2cc520efe9b
Allen Benny <48691514+officiallygod@users.noreply.github.com>
2023-02-27 15:59:40 +0530

    Bump Onboarding to 1.1.2

--- bootstrap.php
+++ bootstrap.php
@@ -19,7 +19,7 @@ function nfd_wp_module_onboarding_register() {
 
 			// Set Global Constants
 			if ( ! defined( 'NFD_ONBOARDING_VERSION' ) ) {
-				define( 'NFD_ONBOARDING_VERSION', '1.1.1' );
+				define( 'NFD_ONBOARDING_VERSION', '1.1.2' );
 			}
 			if ( ! defined( 'NFD_ONBOARDING_DIR' ) ) {
 				define( 'NFD_ONBOARDING_DIR', __DIR__ );
