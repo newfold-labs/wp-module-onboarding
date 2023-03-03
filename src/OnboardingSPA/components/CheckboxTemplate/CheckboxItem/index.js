7fff7690b2a63abbee2417d588863fb6c37ee2b9
arunshenoy99 <devarunshenoy99@gmail.com>
2023-03-03 15:07:15 +0530

    fix lint issues

--- src/OnboardingSPA/components/CheckboxTemplate/CheckboxItem/index.js
+++ src/OnboardingSPA/components/CheckboxTemplate/CheckboxItem/index.js
@@ -23,6 +23,7 @@ const CheckboxItem = ( {
 	desc,
 	subtitle,
 	callback,
+	tabIndex = 0,
 	isSelectedDefault,
 	className = 'checkbox-item',
 } ) => {
@@ -92,6 +93,9 @@ const CheckboxItem = ( {
 						<div
 							className={ `${ className }__contents-help ${ showDescription ? 'highlighted' : '' }` }
 							onClick={ handleShowDesc }
+							role="button"
+							onKeyDown={ handleShowDesc }
+							tabIndex={ tabIndex }
 						>
 							<Icon
 								icon={ help }
