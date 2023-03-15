60c797ad10b39310e84d23e81e5c21d606ff778b
Allen Benny <48691514+officiallygod@users.noreply.github.com>
2023-03-15 19:39:08 +0530

    Adding Abort to API Requests

--- src/OnboardingSPA/utils/api/flow.js
+++ src/OnboardingSPA/utils/api/flow.js
@@ -3,16 +3,31 @@ import { onboardingRestURL } from './common';
 
 import apiFetch from '@wordpress/api-fetch';
 
+let abortController = undefined;
+
 export async function getFlow() {
+	if( abortController ) {
+		abortController.abort();
+	}
+	abortController = new AbortController()
+  	const { signal } = abortController;
+
 	return await resolve(
-		apiFetch( { url: onboardingRestURL( 'flow' ) } ).then()
+		apiFetch( { url: onboardingRestURL( 'flow' ), signal: signal } ).then()
 	);
 }
 
 export async function setFlow( data ) {
+	if( abortController ) {
+		abortController.abort();
+	}
+	abortController = new AbortController()
+  	const { signal } = abortController;
+
 	return await resolve(
 		apiFetch( {
 			url: onboardingRestURL( 'flow' ),
+			signal: signal,
 			method: 'POST',
 			data,
 		} ).then()
