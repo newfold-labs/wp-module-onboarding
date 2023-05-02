0333ab8d78ce823ffcfea17aa1b6e4a7b9104c48
William Earnhardt <wearnhardt@gmail.com>
2023-05-02 18:02:29 -0400

    Clean up redirect handler logic

--- includes/LoginRedirect.php
+++ includes/LoginRedirect.php
@@ -12,49 +12,54 @@ class LoginRedirect {
 	/**
 	 * Handles the redirect to onboarding
 	 *
-	 * @param [type] $redirect The requested redirect URL.
-	 * @param [type] $redirect_to The requested redirect URL via param.
-	 * @param [type] $user The logged in user object.
+	 * @param string $redirect The requested redirect URL
 	 * @return string
 	 */
-	public static function handle_redirect( $redirect, $redirect_to, $user ) {
+	public static function handle_redirect( $redirect ) {
 		$redirect_option_name = Options::get_option_name( 'redirect' );
+		// If request has ?nfd_module_onboarding_redirect=false then temporarily disable the redirect
 		if ( isset( $_GET[ $redirect_option_name ] )
 		  && 'false' === $_GET[ $redirect_option_name ] ) {
 			 self::disable_redirect();
 		}
 
-		$flow_exited    = false;
-		$flow_completed = false;
-		$flow_data      = \get_option( Options::get_option_name( 'flow' ), false );
-		if ( ! empty( $flow_data ) ) {
-			$flow_exited    = ( ! empty( $flow_data['hasExited'] ) );
-			$flow_completed = ( ! empty( $flow_data['isComplete'] ) );
-		}
-
-		if ( \get_transient( Options::get_option_name( 'redirect_param' ) ) === '1'
-		 || $flow_exited
-		 || $flow_completed
-		 ) {
+		// Redirect was temporarily disabled via transient
+		if ( \get_transient( Options::get_option_name( 'redirect_param' ) ) === '1' ) {
 			return $redirect;
 		}
 
+		// Don't redirect if coming soon is off. User has launched their site
+		if ( \get_option( Options::get_option_name( 'coming_soon', false ), 'true' ) !== 'true' ) {
+			return $redirect;
+		}
+
+		// Don't redirect if they have intentionally exited or completed onboarding
+		$flow_data = \get_option( Options::get_option_name( 'flow' ), false );
+		if ( data_get( $flow_data, 'hasExited' ) || data_get( $flow_data, 'isComplete' ) ) {
+			return $redirect;
+		}
+
+		// Check for disabled redirect database option: nfd_module_onboarding_redirect
 		$redirect_option = \get_option( $redirect_option_name, null );
+		// If not set, then set it to true
 		if ( empty( $redirect_option ) ) {
 			$redirect_option = \update_option( $redirect_option_name, true );
 		}
+		if (! $redirect_option) {
+			return $redirect;
+		}
+
+		// If site was created more than 72 hours ago, don't redirect to onboarding
 		$install_date      = new DateTime( \get_option( Options::get_option_name( 'install_date', false ), gmdate( 'M d, Y' ) ) );
 		$current_date      = new DateTime( gmdate( 'M d, Y' ) );
 		$interval          = $current_date->diff( $install_date );
 		$interval_in_hours = ( $interval->days * 24 ) + $interval->h;
-
-		if ( ! ( $redirect_option
-		&& \get_option( Options::get_option_name( 'coming_soon', false ), 'true' ) === 'true'
-		&& ( $interval_in_hours <= 72 ) ) ) {
+		if ( $interval_in_hours >= 72 ) {
 			 return $redirect;
 		}
 
-		if ( self::is_administrator( $user ) ) {
+		// Finally, if we made it this far, then set the redirect URL to point to onboarding if the user is an admin
+		if ( ! current_user_can( 'manage_options' ) ) {
 			 return \admin_url( '/index.php?page=nfd-onboarding' );
 		}
 
