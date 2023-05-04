a9e1aabf229dc12dbfb2c57d0b913389b0e15d7a
arunshenoy99 <devarunshenoy99@gmail.com>
2023-05-04 21:06:56 +0530

    bump onboarding to 1.4.2

---
+++ build/1.4.2/33.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunknewfold_Onboarding=globalThis.webpackChunknewfold_Onboarding||[]).push([[33],{33:(e,a,r)=>{r.r(a),r.d(a,{default:()=>l});var n=r(9307);const l=e=>{let{baseClassName:a="nfd-onboarding-sidebar-learn-more--support-link",link:r="#",text:l}=e;return""!==r&&(0,n.createElement)("a",{href:r,className:a,target:"_blank",rel:"noreferrer"},l)}}}]);
