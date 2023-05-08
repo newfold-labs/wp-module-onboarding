bc36d84f32b1ed6dacbc3e49d666b3044fd5a07e
arunshenoy99 <devarunshenoy99@gmail.com>
2023-05-08 17:55:55 +0530

    fix lint

--- src/OnboardingSPA/components/LivePreview/LivePreviewSkeleton/index.js
+++ src/OnboardingSPA/components/LivePreview/LivePreviewSkeleton/index.js
@@ -1,16 +1,21 @@
 import { useState, useEffect } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 
-import { LivePreviewSelectableCard, LivePreviewSelectableCardWithInfo } from '..';
+import {
+	LivePreviewSelectableCard,
+	LivePreviewSelectableCardWithInfo,
+} from '..';
 
 /**
  * Renders Skeletons for Live Previews.
  *
- * @property {number} count                     The number of Live Previews to be shown
- * @property {number} watch                     The variable to be awaited for
- * @property {string} callback                  The Render function in parent to be called
- * @property {string} className                 The class name for the Live Preview
- * @property {number} viewportWidth             Viewport Width for the Live Preview
- * 
+ * @param {Object}  root0               Props.
+ * @param {number}  root0.count         The number of Live Previews to be shown
+ * @param {number}  root0.watch         The variable to be awaited for
+ * @param {string}  root0.callback      The Render function in parent to be called
+ * @param {string}  root0.className     The class name for the Live Preview
+ * @param {number}  root0.viewportWidth Viewport Width for the Live Preview
+ * @param {boolean} root0.isWithCard    Whether the skeleton is a Card
  */
 const LivePreviewSkeleton = ( {
 	count,
@@ -20,11 +25,10 @@ const LivePreviewSkeleton = ( {
 	viewportWidth,
 	isWithCard = false,
 } ) => {
-
 	const MAX_ANIMATION_TIME = 600000;
-	const [rerender, doRerender] = useState(0);
+	const [ rerender, doRerender ] = useState( 0 );
 
-	useEffect(() => doRerender(1), [watch]);
+	useEffect( () => doRerender( 1 ), [ watch ] );
 
 	const buildDummyPreviews = () => {
 		const dummyPreviews = [];
@@ -46,26 +50,38 @@ const LivePreviewSkeleton = ( {
 	const buildDummyPreviewsWithInfo = () => {
 		const dummyPreviews = [];
 
-		for (let i = 0; i < count; i++) {
+		for ( let i = 0; i < count; i++ ) {
 			dummyPreviews.push(
 				<LivePreviewSelectableCardWithInfo
-						key={ i }
-						className={ className }
-						viewportWidth={ 1200 }
-						styling={ 'custom' }
-						title={ 'Loading...' }
-						description={'Loading...'}
-					/>
+					key={ i }
+					className={ className }
+					viewportWidth={ 1200 }
+					styling={ 'custom' }
+					title={ __( 'Loading…', 'wp-module-onboarding' ) }
+					description={ __( 'Loading…', 'wp-module-onboarding' ) }
+				/>
 			);
 		}
 		return dummyPreviews;
 	};
 
-	return !watch ? (isWithCard ? buildDummyPreviewsWithInfo() : buildDummyPreviews()) : 
-			<>
-				{watch ? <div style={{ display: 'none' }}>{rerender}</div> : null}
-				{callback()}
-			</>;
+	const build = () => {
+		if ( isWithCard ) {
+			return buildDummyPreviewsWithInfo();
+		}
+		return buildDummyPreviews();
+	};
+
+	return ! watch ? (
+		build()
+	) : (
+		<>
+			{ watch ? (
+				<div style={ { display: 'none' } }>{ rerender }</div>
+			) : null }
+			{ callback() }
+		</>
+	);
 };
 
 export default LivePreviewSkeleton;
