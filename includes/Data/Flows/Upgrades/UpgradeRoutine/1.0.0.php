9d15468326fae135904317ca6930978049cdb94d
Yashita <yashita.jain@newfold.com>
2023-05-17 18:54:57 +0530

    lint fix

--- includes/Data/Flows/Upgrades/UpgradeRoutine/1.0.0.php
+++ includes/Data/Flows/Upgrades/UpgradeRoutine/1.0.0.php
@@ -7,7 +7,8 @@
 
 use NewfoldLabs\WP\Module\Onboarding\Data\Options;
 
-if ( $flow_data = \get_option( Options::get_option_name( 'flow' ), false ) ) {
+$flow_data = \get_option( Options::get_option_name( 'flow' ), false );
+if ( $flow_data ) {
 	$flow_data['data']['colorStyle'] = ! empty( $flow_data['data']['palette']['slug'] ) ? $flow_data['data']['palette']['slug'] : '';
 	$flow_data['data']['fontStyle']  = ! empty( $flow_data['data']['typography']['slug'] ) ? $flow_data['data']['typography']['slug'] : '';
 	\update_option( Options::get_option_name( 'flow' ), $flow_data );
