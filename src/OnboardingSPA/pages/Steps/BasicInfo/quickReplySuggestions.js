e452c50066a096399cfd9b2f2525332919e9f4c0
Ajay Yadav <ajay.yadav@newfold.com>
2023-06-12 16:49:49 +0530

    changed map response

--- src/OnboardingSPA/pages/Steps/BasicInfo/quickReplySuggestions.js
+++ src/OnboardingSPA/pages/Steps/BasicInfo/quickReplySuggestions.js
@@ -4,16 +4,15 @@
 import React from '@wordpress/element';
 
 const QuickReplySuggestions = ({ suggestions, onClick }) => {
-
 	return (
 		<div className="quick-reply-wrapper">
 			{suggestions.map((suggestion, index) => (
 				<div
 					className="quick-reply-suggestion"
 					key={index}
-					onClick={() => onClick(suggestion)}
+					onClick={() => onClick(suggestion.text)}
 				>
-					{suggestion}
+					{suggestion.text}
 				</div>
 			))}
 		</div>
