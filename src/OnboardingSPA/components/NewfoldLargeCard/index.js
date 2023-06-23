75e560d963a3c255e670ab354c8536cdcf801984
arunshenoy99 <devarunshenoy99@gmail.com>
2023-06-23 17:22:43 +0530

    fix newfold card logo popping up on safari

--- src/OnboardingSPA/components/NewfoldLargeCard/index.js
+++ src/OnboardingSPA/components/NewfoldLargeCard/index.js
@@ -1,10 +1,12 @@
-import { __ } from '@wordpress/i18n';
 import classNames from 'classnames';
 
-const NewfoldLargeCard = ({ className = '', children }) => {
+const NewfoldLargeCard = ( { className = '', children } ) => {
+	const finalClassName = classNames( 'nfd-onboarding-large-card', className );
+
 	return (
-		<div className={classNames('nfd-onboarding-large-card', className)}>
-			{children}
+		<div className={ finalClassName }>
+			<div className={ `${ finalClassName }__logo` }></div>
+			{ children }
 		</div>
 	);
 };
