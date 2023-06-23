f5f5459fedd15b5144a62bac68822688f8ca5746
arunshenoy99 <devarunshenoy99@gmail.com>
2023-06-23 17:50:19 +0530

    use a static classname

--- src/OnboardingSPA/components/NewfoldLargeCard/index.js
+++ src/OnboardingSPA/components/NewfoldLargeCard/index.js
@@ -1,11 +1,9 @@
 import classNames from 'classnames';
 
 const NewfoldLargeCard = ( { className = '', children } ) => {
-	const finalClassName = classNames( 'nfd-onboarding-large-card', className );
-
 	return (
-		<div className={ finalClassName }>
-			<div className={ `${ finalClassName }__logo` }></div>
+		<div className={ classNames( 'nfd-onboarding-large-card', className ) }>
+			<div className={ `nfd-onboarding-large-card__logo` }></div>
 			{ children }
 		</div>
 	);
