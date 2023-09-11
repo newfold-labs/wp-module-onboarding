a8d9935d412425d40ebac368badeac6f14204b6a
Allen Benny <allen.officially@gmail.com>
2023-09-11 17:47:36 +0530

    Fix JS-Doc

--- src/OnboardingSPA/components/Grid/index.js
+++ src/OnboardingSPA/components/Grid/index.js
@@ -1,15 +1,20 @@
-import classNames from "classnames";
+import classNames from 'classnames';
 
 /**
  * Grid Component
  * Shows the components in a grid view
  *
- * @return Grid of children
+ * @param {Object} root0
+ * @param {string} root0.className
+ * @param {number} root0.size
+ * @param {number} root0.colGap
+ * @param {Object} root0.children
+ * @return {WPComponent} NFDOnboarding Grid
  */
 const Grid = ( { className = '', size, colGap = 0, children } ) => {
 	return (
 		<div
-			className={ classNames('nfd-onboarding-grid', className) }
+			className={ classNames( 'nfd-onboarding-grid', className ) }
 			style={ {
 				gridTemplateColumns: `repeat(${ size }, 1fr)`,
 				gridColumnGap: `${ colGap }px`,
