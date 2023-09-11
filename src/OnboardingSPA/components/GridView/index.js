ba60f967a22595cc1389c2e5b60d34d880ad23f0
Allen Benny <allen.officially@gmail.com>
2023-09-11 17:10:02 +0530

    Using Inline CSS

--- src/OnboardingSPA/components/GridView/index.js
+++ src/OnboardingSPA/components/GridView/index.js
@@ -1,7 +1,6 @@
 /**
  * Common Grid View Component
  * Shows the components in a grid view
- * Supports column size 2 - 5
  *
  * @return GridView of children
  */
@@ -9,8 +8,11 @@
 const GridView = ( { size, colGap = 0, children } ) => {
 	return (
 		<div
-			className={ `grid-basic grid-col-${ size }` }
-			style={ { gridColumnGap: `${ colGap }px` } }
+			className={ `grid-basic` }
+			style={ {
+				gridTemplateColumns: `repeat(${ size }, 1fr)`,
+				gridColumnGap: `${ colGap }px`,
+			} }
 		>
 			{ children }
 		</div>
