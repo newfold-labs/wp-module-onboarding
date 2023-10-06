ca95aba394fc0225f38d13f9b6dbe89f6b78c48c
Allen Benny <allen.officially@gmail.com>
2023-10-06 13:46:15 +0530

    Change endpoint to own backend.

--- src/OnboardingSPA/utils/api/comingSoon.js
+++ src/OnboardingSPA/utils/api/comingSoon.js
@@ -1,15 +1,16 @@
+import { onboardingRestURL } from './common';
 import { resolve } from './resolve';
-import { wpSiteUrl } from '../../../constants';
 
 import apiFetch from '@wordpress/api-fetch';
 
 export async function setComingSoon( comingSoon ) {
-	const data = { comingSoon };
 	return await resolve(
 		apiFetch( {
-			url: wpSiteUrl + '/index.php?rest_route=/bluehost/v1/settings',
+			url: onboardingRestURL( 'settings/coming-soon' ),
 			method: 'POST',
-			data,
+			data: {
+				comingSoon,
+			},
 		} ).then()
 	);
 }
