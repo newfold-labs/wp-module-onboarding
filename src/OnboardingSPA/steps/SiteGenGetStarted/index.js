0c4f25a2c0f120e67f1ec315a50b79dbbf82fbf8
Ajay Yadav <ajay.yadav@newfold.com>
2023-10-23 12:27:15 +0530

    PRESS2-1233 | fixed linting changes

--- src/OnboardingSPA/steps/SiteGenGetStarted/index.js
+++ src/OnboardingSPA/steps/SiteGenGetStarted/index.js
@@ -1,25 +1,8 @@
-import { useViewportMatch } from '@wordpress/compose';
-import { useEffect } from '@wordpress/element';
-import { useDispatch } from '@wordpress/data';
-
-import { store as nfdOnboardingStore } from '../../store';
 import CommonLayout from '../../components/Layouts/Common';
 import HeadingWithSubHeading from './headingwithsubheading';
 import getContents from './contents';
 
 const StepSiteGenGetStarted = () => {
-	const isLargeViewport = useViewportMatch( 'medium' );
-
-	const { setIsDrawerOpened, setIsHeaderNavigationEnabled } =
-		useDispatch( nfdOnboardingStore );
-
-	useEffect( () => {
-		if ( isLargeViewport ) {
-			setIsDrawerOpened( true );
-		}
-		setIsHeaderNavigationEnabled( true );
-	}, [] );
-
 	const content = getContents();
 
 	return (
