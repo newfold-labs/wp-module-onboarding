5a093bd3824dde746a9f81fabe4fcd18bc219610
Allen Benny <allen.officially@gmail.com>
2023-10-31 18:25:03 +0530

    Update index.js

--- src/OnboardingSPA/steps/SiteFeatures/index.js
+++ src/OnboardingSPA/steps/SiteFeatures/index.js
@@ -13,7 +13,6 @@ import CheckboxList from '../../components/CheckboxTemplate/CheckboxList';
 import { CheckboxListSkeleton } from '../../components/CheckboxTemplate';
 import getContents from './contents';
 import ComingSoon from '../../components/ComingSoon';
-import ExperienceLevel from '../../../SiteGenSPA/steps/ExperienceLevel';
 
 const StepSiteFeatures = () => {
 	const isLargeViewport = useViewportMatch( 'medium' );
@@ -92,7 +91,30 @@ const StepSiteFeatures = () => {
 	const content = getContents();
 
 	return (
-		<ExperienceLevel/>
+		<CommonLayout>
+			<div className="site-features__heading">
+				<HeadingWithSubHeading
+					title={ content.heading }
+					subtitle={ content.subheading }
+				/>
+			</div>
+			{ ! customPluginsList && (
+				<CheckboxListSkeleton
+					count={
+						themeVariations[ currentStep?.data?.patternId ]
+							?.previewCount
+					}
+				/>
+			) }
+			{ customPluginsList && (
+				<CheckboxList
+					callback={ selectPlugin }
+					selectedItems={ selectedPlugins }
+					customItemsList={ customPluginsList }
+				/>
+			) }
+			{ customPluginsList && <ComingSoon /> }
+		</CommonLayout>
 	);
 };
 
