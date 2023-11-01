ec691568cfae0a86f311ae252b9f70a11f93de80
Ajay Yadav <ajay.yadav@newfold.com>
2023-11-01 12:36:12 +0530

    PRESS2-1259 | fetching users data to display in admin bar

--- src/SiteGenSPA/components/AdminBar/index.js
+++ src/SiteGenSPA/components/AdminBar/index.js
@@ -1,7 +1,26 @@
 import { __ } from '@wordpress/i18n';
 import { Icon, wordpress } from '@wordpress/icons';
+import apiFetch from '@wordpress/api-fetch';
+import { useState, useEffect } from '@wordpress/element';
 
 const AdminBar = () => {
+	const [ userData, setUserData ] = useState( null );
+
+	useEffect( () => {
+		apiFetch( { path: '/wp/v2/users/me' } )
+			.then( ( data ) => {
+				setUserData( data );
+			} )
+			.catch( ( error ) => {
+				// eslint-disable-next-line no-console
+				console.error( 'Error fetching user data:', error );
+			} );
+	}, [] );
+
+	if ( ! userData ) {
+		return null;
+	}
+
 	return (
 		<div className="nfd-sitegen-admin-bar">
 			<div className="nfd-admin-bar-wplogo">
@@ -11,10 +30,16 @@ const AdminBar = () => {
 			<div className="nfd-sitegen-admin-bar-profile">
 				<span className="nfd-admin-bar-greeting">
 					<span>
-						{ __( 'Howdy!', 'wp-module-onboarding' ) }, Maya Jiménez
+						{ __( 'Howdy!', 'wp-module-onboarding' ) },{ ' ' }
+						{ userData.name }
 					</span>
 				</span>
-				<div className="nfd-admin-bar-avatar"></div>
+				<div className="nfd-admin-bar-avatar">
+					<img
+						src={ userData.avatar_urls[ '24' ] }
+						alt={ userData.name }
+					/>
+				</div>
 			</div>
 		</div>
 	);
