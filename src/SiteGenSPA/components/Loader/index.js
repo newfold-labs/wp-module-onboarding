96117529e7e354e6c1d8b951dbce2917cad3fb2a
Allen Benny <allen.officially@gmail.com>
2023-11-01 13:17:59 +0530

    Code Dump

--- src/SiteGenSPA/components/Loader/index.js
+++ src/SiteGenSPA/components/Loader/index.js
@@ -10,10 +10,17 @@ const Loader = () => {
 			<div className={ addThemeSuffix( 'nfd-sg-loader__title' ) }>
 				{ content.title }
 			</div>
-			<div
-				className={ addThemeSuffix( 'nfd-sg-loader__progress' ) }
-				style={ { width: `${ 320 * percentage * 0.01 }px` } }
-			/>
+			<div className={ addThemeSuffix( 'nfd-sg-loader__progress' ) }>
+				<div
+					className={ addThemeSuffix( 'nfd-sg-loader__progress_bg' ) }
+				/>
+				<div
+					className={ addThemeSuffix(
+						'nfd-sg-loader__progress_bar'
+					) }
+					style={ { width: `${ 320 * percentage * 0.01 }px` } }
+				/>
+			</div>
 			<div className={ addThemeSuffix( 'nfd-sg-loader__status' ) }></div>
 		</div>
 	);
