d8a29baa3be6d11e3959d973c90061529ed67501
Allen Benny <allen.officially@gmail.com>
2023-11-09 14:01:37 +0530

    Update scss

--- src/OnboardingSPA/steps/SiteGen/SiteDetails/index.js
+++ src/OnboardingSPA/steps/SiteGen/SiteDetails/index.js
@@ -1,15 +1,14 @@
-import CommonLayout from '../../../components/Layouts/Common';
-
 import { useDispatch } from '@wordpress/data';
-import { HEADER_SITEGEN } from '../../../../constants';
 import { useEffect, useState } from '@wordpress/element';
-import { store as nfdOnboardingStore } from '../../../store';
 
 import getContents from './contents';
+import Animate from '../../../components/Animate';
 import AIHeading from '../../../components/AIHeading';
+import { HEADER_SITEGEN } from '../../../../constants';
+import { store as nfdOnboardingStore } from '../../../store';
+import CommonLayout from '../../../components/Layouts/Common';
 import TextInputSiteGen from '../../../components/TextInput/TextInputSiteGen';
 import NextButtonSiteGen from '../../../components/Button/NextButtonSiteGen';
-import Animate from '../../../components/Animate';
 
 const SiteGenSiteDetails = () => {
 	const content = getContents();
