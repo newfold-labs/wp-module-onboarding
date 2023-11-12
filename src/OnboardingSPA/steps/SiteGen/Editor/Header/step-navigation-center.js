749584cc0e5db1767a045b395c5b7c94e7a81442
diDroid <diwanshu.joshi@newfold.com>
2023-11-12 16:31:37 +0530

    step editor drop down wip

--- src/OnboardingSPA/steps/SiteGen/Editor/Header/step-navigation-center.js
+++ src/OnboardingSPA/steps/SiteGen/Editor/Header/step-navigation-center.js
@@ -1,49 +1,80 @@
-import { useSelect } from '@wordpress/data';
 import { Icon, chevronDown } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 
-import { store as nfdOnboardingStore } from '../../../../store';
 import ButtonDark from '../../../../components/Button/ButtonDark';
 import {ReactComponent as Wishlist} from '../../../../static/icons/site-features/wishlist.svg'
+import { Button, Dropdown } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 
-/**
- * Back step Navigation button.
- *
- * @param {*} param0
- *
- * @return {WPComponent} Back Component
- */
-const VersionDropDown = ( ) => {
-	const version = () => { 
-	};
+const onTextChange = ( e ) => {
+	e.preventDefault();
+	setCustomerInput( e.target.value );
+};
+
+
+const TextInput = ( { customerInput, isDisabled } ) => {
 	return (
-		<ButtonDark onClick={ version } variant="secondary">
-			<Wishlist />
-			{ __( 'Version 1', 'wp-module-onboarding' ) }
-			<Icon icon={ chevronDown } />
-		</ButtonDark>
+		<input
+			className='nfd-onboarding-header__center-input'
+			disabled = { isDisabled }
+			type="text"
+			value = { customerInput }
+			onChange={ ( e ) => onTextChange( e ) }
+		/>
 	);
 };
 
 /**
- * Step buttons presented in Header.
+ * Version step Navigation button.
+ *
+ * @return {WPComponent} VersionButton Component
+ */
+const VersionDropDown = ( ) => {
+	return (
+		<div className='nfd-onboarding-header__center-dropdown-item'>
+			<Button>Rename</Button>
+			<Button>View All</Button>
+		</div>
+	);
+};
+
+/**
+ * Version step Navigation button.
+ *
+ * @return {WPComponent} VersionButton Component
+ */
+const VersionButton = ( {isInputDisabled} ) => {
+	return (
+		<Dropdown
+			popoverProps={ { placement: 'bottom-start' } }
+			renderToggle={ ( { isOpen, onToggle } ) => (
+				<ButtonDark
+					onClick={ onToggle }
+					aria-expanded={ isOpen }
+				>
+					<Wishlist />
+					{/* { __( 'Version 1', 'wp-module-onboarding' ) } */}
+					<TextInput customerInput={  __( 'Version 1', 'wp-module-onboarding' ) } disabled={isInputDisabled} />
+					<Icon icon={ chevronDown } />
+				</ButtonDark>
+			) }
+			renderContent={ () => <VersionDropDown />
+			}
+    	/>
+	);
+};
+
+/**
+ * Centre Step buttons presented in Header.
  *
  * @return {WPComponent} StepNavigation Component
  */
 const StepNavigationCenter = () => {
-	const { previousStep, showErrorDialog } = useSelect( ( select ) => {
-		return {
-			previousStep: select( nfdOnboardingStore ).getPreviousStep(),
-			showErrorDialog: select( nfdOnboardingStore ).getShowErrorDialog(),
-		};
-	}, [] );
+	const [isInputDisabled, setIsInputDisabled] = useState(true);
 	
 	return (
 		<div className="nfd-onboarding-header__step-navigation">
-			<VersionDropDown
-				path={ previousStep.path }
-				showErrorDialog={ showErrorDialog }
-			/>
+			<VersionButton isInputDisabled={isInputDisabled}/>
 		</div>
 	);
 };
