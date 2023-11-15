348c37f715ce02c768305c7576593f5ed23bdce0
girish-lokapure <lokapure.girish@gmail.com>
2023-11-15 18:36:09 +0530

    initial commit

---
+++ src/OnboardingSPA/steps/SiteGen/SocialMedia/contents.js
@@ -0,0 +1,24 @@
+import { __ } from '@wordpress/i18n';
+
+const getContents = () => {
+	return {
+		heading: __(
+			'Do you want to include any content from Facebook?',
+			'wp-module-onboarding'
+		),
+		facebookTitle: __(
+			'Connect a Facebook Account',
+			'wp-module-onboarding'
+		),
+		facebookdesc: __(
+			'By connecting a Facebook profile, we can fetch relevant data to increase the accuracy of your Al generated site.',
+			'wp-module-onboarding'
+		),
+		facebookbutton: __( 'Connect Facebook', 'wp-module-onboarding' ),
+		buttons: {
+			skip: __( 'Skip for now', 'wp-module-onboarding' ),
+		},
+	};
+};
+
+export default getContents;
--- src/OnboardingSPA/steps/SiteGen/SocialMedia/index.js
+++ src/OnboardingSPA/steps/SiteGen/SocialMedia/index.js
@@ -5,8 +5,10 @@ import { useEffect } from '@wordpress/element';
 import { useDispatch } from '@wordpress/data';
 import { store as nfdOnboardingStore } from '../../../store';
 import { HEADER_SITEGEN } from '../../../../constants';
-
-import SiteGenPlaceholder from '../../../components/SiteGenPlaceholder';
+import getContents from './contents';
+import ButtonNext from '../../../components/Button/ButtonNext';
+import SkipButton from '../../../components/SkipButton';
+import ImageUploaderWithText from '../../../components/ImageUploader/components/ImageUploaderWithText';
 
 const SiteGenSiteSocialMedia = () => {
 	const {
@@ -22,12 +24,40 @@ const SiteGenSiteSocialMedia = () => {
 		setHeaderActiveView( HEADER_SITEGEN );
 		setDrawerActiveView( false );
 	} );
+
+	const content = getContents();
 	return (
 		<CommonLayout
 			isCentered
 			className="nfd-onboarding-step--site-gen__social-media"
 		>
-			<SiteGenPlaceholder heading={ 'Social Media' } />
+			<div className="nfd-onboarding-step--site-gen__social-media__container">
+				<div className="nfd-onboarding-step--site-gen__social-media__container__heading">
+					<div className="nfd-onboarding-step--site-gen__social-media__container__heading__animation"></div>
+					<p className="nfd-onboarding-step--site-gen__social-media__container__heading__text">
+						{ content.heading }
+					</p>
+				</div>
+				<div className="nfd-onboarding-step--site-gen__social-media__contain ">
+					<div className="nfd-onboarding-step--site-gen__social-media__contain__containleft ">
+						{ content.facebookTitle }
+						<p>{ content.facebookdesc }</p>
+					</div>
+					<div className="nfd-onboarding-step--site-gen__social-media__contain__containright ">
+						<button className="nfd-onboarding-step--site-gen__social-media__contain__containright__button ">
+						<i></i>{ content.facebookbutton }
+						</button>
+					</div>
+				</div>
+				<div className="nfd-onboarding-step--site-gen__social-media__container__buttons">
+					<SkipButton
+						callback={ () => resetSiteLogo() }
+						className="nfd-onboarding-step--site-gen__social-media__container__buttons__skip"
+						text={ content.buttons.skip }
+					/>
+					<ButtonNext disabled={ true } />
+				</div>
+			</div>
 		</CommonLayout>
 	);
 };
