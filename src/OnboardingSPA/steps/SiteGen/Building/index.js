234c407ad8ebe4157b72af428270cd07bfeaaf9d
Allen Benny <allen.officially@gmail.com>
2023-11-16 11:43:27 +0530

    Add Animation

--- src/OnboardingSPA/steps/SiteGen/Building/index.js
+++ src/OnboardingSPA/steps/SiteGen/Building/index.js
@@ -28,11 +28,11 @@ const SiteGenBuilding = () => {
 			className="nfd-onboarding-step--site-gen__building"
 		>
 			<div className="site-gen__building_skimmer">
-				<div className="site-gen__building_skimmer--header"></div>
-				<div className="site-gen__building_skimmer--body"></div>
+				<div className="site-gen__building_skimmer--main site-gen__building_skimmer--header"></div>
+				<div className="site-gen__building_skimmer--main site-gen__building_skimmer--body"></div>
 				<div className="site-gen__building_skimmer--footer">
-					<div className="site-gen__building_skimmer--footer_left"></div>
-					<div className="site-gen__building_skimmer--footer_right"></div>
+					<div className="site-gen__building_skimmer--main site-gen__building_skimmer--footer_left"></div>
+					<div className="site-gen__building_skimmer--main site-gen__building_skimmer--footer_right"></div>
 				</div>
 			</div>
 			<div className="site-gen__building_loader__overlay">
