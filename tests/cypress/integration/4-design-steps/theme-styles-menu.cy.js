842b1d10653fd2f2c624620ee7367a3866ba1015
Avneet Raj <razavneet@gmail.com>
2023-11-22 15:09:38 +0530

    addressing review comments

--- tests/cypress/integration/4-design-steps/theme-styles-menu.cy.js
+++ tests/cypress/integration/4-design-steps/theme-styles-menu.cy.js
@@ -8,7 +8,7 @@ import {
 	CheckIntroPanel,
 } from '../wp-module-support/sidebar.cy';
 
-describe( 'Theme Styles Menu and Preview', function () {
+describe( 'Theme Styles Menu', function () {
 	before( () => {
 		cy.visit(
 			'wp-admin/?page=nfd-onboarding#/wp-setup/step/design/theme-styles/menu'
@@ -51,74 +51,5 @@ describe( 'Theme Styles Menu and Preview', function () {
 				.should( 'be.visible' );
 			previewCount += 1;
 		} );
-	} );
-
-	it.skip( 'Check if Theme is selected and switches to Preview page', () => {
-		cy.get(
-			':nth-child(2) > .theme-styles-menu__list__item__live-preview-container'
-		)
-			.scrollIntoView()
-			.should( 'be.visible' )
-			.click();
-		cy.url().should(
-			'not.include',
-			'#/wp-setup/step/design/theme-styles/menu'
-		);
-		cy.get( ':nth-child(1) > .theme-styles-preview__title-bar' ).should(
-			'be.visible'
-		);
-		cy.get( '.theme-styles-preview > div:nth-child(3)' ).should(
-			'be.visible'
-		);
-	} );
-
-	it.skip( 'Check if Theme List is Visible in the Drawer', () => {
-		let previewCount = 0;
-		const className = '.theme-styles-preview--drawer__list__item';
-		const arr = cy.get( className );
-
-		arr.each( () => {
-			cy.get( className.concat( '__title-bar' ) )
-				.eq( previewCount )
-				.scrollIntoView()
-				.should( 'be.visible' );
-			cy.get( className.concat( '__live-preview-container' ) )
-				.eq( previewCount )
-				.scrollIntoView()
-				.should( 'be.visible' );
-			previewCount += 1;
-		} );
-	} );
-
-	it.skip( 'Check for the selected theme in Drawer', () => {
-		cy.get( '.nfd-onboarding-drawer__panel-back' )
-			.scrollIntoView()
-			.should( 'be.visible' )
-			.should( 'have.text', 'Design' );
-		const className =
-			':nth-child(2) > .theme-styles-preview--drawer__list__item__title-bar';
-		cy.get( className );
-		cy.get( className + ' > .live-preview-selected-check' )
-			.scrollIntoView()
-			.should( 'be.visible' );
-	} );
-
-	it.skip( 'Check/uncheck Colors and Font Checkbox', () => {
-		cy.get( '[type=checkbox]' ).click().should( 'be.checked' );
-		cy.get( '[type=checkbox]' ).click().should( 'not.be.checked' );
-	} );
-
-	it.skip( 'Check for selected Theme in preview when navigated back to Menu page', () => {
-		cy.get( '.navigation-buttons_back' ).click();
-		cy.url().should(
-			'not.include',
-			'#/wp-setup/step/design/theme-styles/preview'
-		);
-		const className =
-			':nth-child(2) > .theme-styles-menu__list__item__title-bar';
-		cy.get( className );
-		cy.get( className + ' > .live-preview-selected-check' ).should(
-			'be.visible'
-		);
 	} );
 } );
