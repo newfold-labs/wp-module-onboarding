af1e3318377f83a91db6a96251792aed31e54a9c
Allen Benny <allen.officially@gmail.com>
2023-11-24 15:25:15 +0530

    Update siteGen.js

--- src/OnboardingSPA/utils/api/siteGen.js
+++ src/OnboardingSPA/utils/api/siteGen.js
@@ -3,16 +3,20 @@ import apiFetch from '@wordpress/api-fetch';
 import { resolve } from './resolve.js';
 import { onboardingRestURL } from './common';
 
-export async function generateSiteGenMeta( site_info, identifier, skip_cache = false ) {
+export async function generateSiteGenMeta(
+	site_info,
+	identifier,
+	skip_cache = false
+) {
 	return await resolve(
 		apiFetch( {
 			url: onboardingRestURL( 'sitegen/generate' ),
 			method: 'POST',
 			data: {
-                site_info,
-                identifier,
-                skip_cache,
-            },
+				site_info,
+				identifier,
+				skip_cache,
+			},
 		} ).then()
 	);
 }
