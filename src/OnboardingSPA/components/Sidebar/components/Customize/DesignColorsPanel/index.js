e0aab336e49bceee994b40fa1b7e92a3253057c6
Vara <varaprasadmaruboina@gmail.com>
2023-12-05 23:06:00 +0530

    Update customize sidebar

--- src/OnboardingSPA/components/Sidebar/components/Customize/DesignColorsPanel/index.js
+++ src/OnboardingSPA/components/Sidebar/components/Customize/DesignColorsPanel/index.js
@@ -1,166 +1,64 @@
 import React, { useState } from 'react';
-import { PanelBody, PanelRow, Button, Popover, ColorPicker, ColorPalette } from '@wordpress/components';
+import { PanelBody, PanelRow, Button } from '@wordpress/components';
 import ColorPickerButton from '../../../../ColorPickerButton';
+import { design, colorPalettes } from '../data';
+import ColorPaletteIcon from './ColorPaletteIcon'
+import CustomColorPalette from './CustomColorPalette'
+import './stylesheet.scss';
 
-
-const MyColorPalette = ({onChange}) => {
-	const [ color, setColor ] = useState ( '#f00' )
-	const colors = [
-    {
-      colors: [
-        {
-          color: '#f00',
-          name: 'Red'
-        },
-        {
-          color: '#ff0',
-          name: 'Yellow'
-        },
-        {
-          color: '#00f',
-          name: 'Blue'
-        },        {
-          color: '#f00',
-          name: 'Red'
-        },
-        {
-          color: '#ff0',
-          name: 'Yellow'
-        },
-        {
-          color: '#00f',
-          name: 'Blue'
-        },        {
-          color: '#f00',
-          name: 'Red'
-        },
-        {
-          color: '#ff0',
-          name: 'Yellow'
-        },
-        {
-          color: '#00f',
-          name: 'Blue'
-        },
-        {
-          color: '#00f',
-          name: 'Blue'
-        }
-      ],
-      name: 'Primary colors'
-    },
-    {
-      colors: [
-        {
-          color: '#f00',
-          name: 'Red'
-        },
-        {
-          color: '#ff0',
-          name: 'Yellow'
-        },
-        {
-          color: '#00f',
-          name: 'Blue'
-        },        {
-          color: '#f00',
-          name: 'Red'
-        },
-        {
-          color: '#ff0',
-          name: 'Yellow'
-        },
-        {
-          color: '#00f',
-          name: 'Blue'
-        },        {
-          color: '#f00',
-          name: 'Red'
-        },
-        {
-          color: '#ff0',
-          name: 'Yellow'
-        },
-        {
-          color: '#00f',
-          name: 'Blue'
-        },
-        {
-          color: '#00f',
-          name: 'Blue'
-        },
-        {
-          color: '#00f',
-          name: 'Blue'
-        },
-        {
-          color: '#00f',
-          name: 'Blue'
-        }
-      ],
-      name: 'Secondary colors'
-    }
-  ];
-  const handleColorChange = (newColor) => {
-    setColor(newColor);
-    onChange(newColor); 
-  };
-	return (
-		<ColorPalette
-			colors={ colors }
-			value={ color }
-      onChange={handleColorChange} 
-		/>
-	);
-};
 const DesignColorsPanel = ({
   baseClassName = 'nfd-onboarding-sidebar-customize--design-colors-panel',
   heading,
-  subheading,
-  icon,
 }) => {
+
+  const palettePrimaryColors = Object.entries(design.color_palette).map(([name, color]) => ({
+    name: 'Custom',
+    color
+  }));
+
+  const defaultColors = {
+    primary: design.color_palette.primary,
+    secondary: design.color_palette.secondary,
+    tertiary: design.color_palette.tertiary
+  }
+  const palette1 = {
+    primary: colorPalettes[1].primary,
+    secondary: colorPalettes[1].secondary,
+    tertiary: colorPalettes[1].tertiary
+  }
+  const palette2 = {
+    primary: colorPalettes[2].primary,
+    secondary: colorPalettes[2].secondary,
+    tertiary: colorPalettes[2].tertiary
+  }
+  const palette3 = {
+    primary: colorPalettes[3].primary,
+    secondary: colorPalettes[3].secondary,
+    tertiary: colorPalettes[3].tertiary
+  }
+
   const [colors, setColors] = useState([
-    { primary: '#ece9e4', secondary: '#1e1f27', tertiary: '#6f8598' },
-    { primary: '#0077cc', secondary: '#66aaff', tertiary: '#aaccff' },
-    { primary: '#996633', secondary: '#cc9900', tertiary: '#663300' },
-    { primary: '#ffcc99', secondary: '#ff9966', tertiary: '#ffeecc' },
+    defaultColors,
+    palette1,
+    palette2,
+    palette3,
   ]);
-  const [selectedColor, setSelectedColor] = useState({
-    primary: '#0077cc',
-    secondary: '#66aaff',
-    tertiary: '#aaccff',
-  });
+
+  const [selectedColor, setSelectedColor] = useState(palette1);
   const [showCustomColors, setShowCustomColors] = useState(false);
   const [selectedPalette, setSelectedPalette] = useState(1);
   const [colorPickerCalledBy, setColorPickerCalledBy] = useState('');
   const [showColorPicker, setShowColorPicker] = useState(false);
 
-  const ColorPalette = ({ idx, label }) => {
-    const conicGradient = `conic-gradient(${colors[idx]['primary']} 90deg, ${colors[idx]['secondary']} 90deg 150deg, ${colors[idx]['tertiary']} 150deg 330deg, ${colors[idx]['primary']} 330deg 360deg)`;
-
-    const handleClick = () => {
-      setSelectedPalette(idx);
-      setSelectedColor(colors[idx]);
-    };
-    return (
-      <div style={{ textAlign: 'center' }}>
-        <div
-          className="color-palette"
-          style={{
-            boxSizing: 'border-box',
-            boxShadow: selectedPalette == idx ? '0 0 0 2px #FFF, 0 0 0 4px #035FEF' : 'none',
-            width: '45px',
-            height: '45px',
-            borderRadius: '50%',
-            background: conicGradient,
-            cursor: 'pointer',
-          }}
-          onClick={handleClick}
-        ></div>
-        <p style={{ marginTop: '8px', color: 'grey' }}>{label}</p>
-      </div>
-    );
-  };
+  const paletteSecondaryColors = Object.entries(colorPalettes[1]).map(([name, color]) => {
+    if (name !== "name") {
+      return {
+        name: 'Custom',
+        color
+      };
+    }
+    return null;
+  }).filter(Boolean);
 
   const handleApplyCustomColors = () => {
     colors[selectedPalette] = selectedColor;
@@ -186,9 +84,16 @@ const DesignColorsPanel = ({
             <p className={`${baseClassName}__container__text__heading`}>
               <strong>{heading}</strong>
             </p>
-            <div style={{ display: 'flex', justifyContent: 'space-around', gap: '20px' }}>
+            <div className={`${baseClassName}__container__color__palette__icon`}>
               {Object.keys(colors).map((idx) => (
-                <ColorPalette key={idx} idx={idx} label={idx == 0 ? 'Default' : ''} />
+                <ColorPaletteIcon
+                  key={idx}
+                  idx={idx}
+                  label={idx == 0 ? 'Default' : ''}
+                  selectedPalette={selectedPalette}
+                  setSelectedPalette={setSelectedPalette}
+                  setSelectedColor={setSelectedColor}
+                  colors={colors} />
               ))}
             </div>
           </div>
@@ -196,66 +101,42 @@ const DesignColorsPanel = ({
       </PanelRow>
       <PanelRow>
         {showCustomColors ? (
-          <div style={{ width: '100%' }}>
+          <div className={`${baseClassName}__custom__colors__container`}>
             <h5 className={`${baseClassName}__heading`}>CUSTOM COLORS</h5>
-            <div style={{ width: '100%' }}>
-              <div style={{ width: '100%' }}>
-                {['primary', 'secondary', 'tertiary'].map((colorType, index) => (
-                  <ColorPickerButton
-                    key={index}
-                    isColorSelected={selectedColor}
-                    color={selectedColor[colorType]}
-                    slug={colorType}
-                    name={colorType.charAt(0).toUpperCase() + colorType.slice(1)}
-                    callback={handleColorPickerButton}
-                  />
-                ))}
-              </div>
-              <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'flex-end' }}>
-                <Button onClick={() => setShowCustomColors(false)} style={{ color: '#44494d' }}>
-                  Cancel
-                </Button>
-                <Button
-                  onClick={handleApplyCustomColors}
-                  variant="primary"
-                  style={{ backgroundColor: '#44494d', borderRadius: '5px' }}
-                >
-                  Apply
-                </Button>
-              </div>
+            <div>
+              {['primary', 'secondary', 'tertiary'].map((colorType, index) => (
+                <ColorPickerButton
+                  key={index}
+                  isColorSelected={selectedColor}
+                  color={selectedColor[colorType]}
+                  slug={colorType}
+                  name={colorType.charAt(0).toUpperCase() + colorType.slice(1)}
+                  callback={handleColorPickerButton}
+                />
+              ))}
             </div>
-            {showColorPicker && (
-              <Popover>
-                <div style={{padding: '16px', width: '260px'}}>
-    
-                  <MyColorPalette onChange={handleColorPicker} />
-                </div>
+            <div className={`${baseClassName}__custom__colors__container__buttons`}>
+              <Button onClick={() => setShowCustomColors(false)} className={'cancel'}>
+                Cancel
+              </Button>
+              <Button
+                onClick={handleApplyCustomColors}
+                variant="primary"
+              >
+                Apply
+              </Button>
+            </div>
 
-                {/* <div
-                  role="button"
-                  tabIndex={0}
-                  className="custom-palette__picker__close-icon"
-                  onClick={() => setShowColorPicker(false)}
-                  onKeyDown={() => setShowColorPicker(false)}
-                >
-                  X
-                </div>
-                <ColorPicker onChange={handleColorPicker} /> */}
-              </Popover>
+            {showColorPicker && (
+              <CustomColorPalette
+                onChange={handleColorPicker}
+                palettePrimaryColors={palettePrimaryColors}
+                paletteSecondaryColors={paletteSecondaryColors} />
             )}
           </div>
         ) : (
-          <div style={{ width: '100%', textAlign: 'center' }}>
-            <Button
-              style={{
-                border: '1px solid #9ca2a7',
-                borderRadius: '8px',
-                width: '100%',
-                display: 'block',
-                margin: '0 auto',
-              }}
-              onClick={() => setShowCustomColors(true)}
-            >
+          <div className={`${baseClassName}__custom__colors__button__container`}>
+            <Button onClick={() => setShowCustomColors(true)} >
               Pick your own colors
             </Button>
           </div>
--- src/OnboardingSPA/components/Sidebar/components/Customize/DesignFontsPanel/index.js
+++ src/OnboardingSPA/components/Sidebar/components/Customize/DesignFontsPanel/index.js
@@ -1,54 +1,54 @@
 import React, { useState } from 'react';
 import { PanelBody, PanelRow, Button, Dashicon } from '@wordpress/components';
+import './stylesheet.scss';
+import { design, designStyles } from '../data';
 
-const FontGroup = ({ group, selectedGroup, handleGroupSelect }) => (
-	<div key={group.id} style={{ marginBottom: '20px', display: 'flex', alignItems: 'flex-start', width: '100%' }}>
-		<div role="button" style={{ cursor: 'pointer', display: 'flex', alignItems: 'flex-start', marginLeft: '0px', width: '100%' }} onClick={() => handleGroupSelect(group.id)}>
-			<Dashicon icon={'yes-alt'} size={30} style={{ marginRight: '10px', color: selectedGroup === group.id ? '#0160F0' : '#9ca2a7' }} />
-			<div style={{ marginLeft: '10px', flex: 1 }}>
-				<span style={{ fontSize: '18px', fontFamily: group.headings, marginBottom: '5px' }}>{group.headings}</span><br />
-				<span style={{ fontFamily: group.body, marginBottom: '5px' }}>{group.body}</span>
+const FontGroup = ({ baseClassName, group, selectedGroup, handleGroupSelect }) => (
+	<div className={`${baseClassName}__font-group__container`} key={group.id} >
+		<div className={`${baseClassName}__font-group__container__button`} role="button" onClick={() => handleGroupSelect(group.id)}>
+			<Dashicon className={`${baseClassName}__font-group__container__button__icon`} icon={'yes-alt'} size={30} style={{ color: selectedGroup === group.id ? '#0160F0' : '#9ca2a7' }} />
+			<div className={`${baseClassName}__font-group__container__button__font-name__container`} >
+				<span className={`${baseClassName}__font-group__container__button__font-name__container__heading`} style={{ fontFamily: group.headings }}>{group.headings}</span><br />
+				<span className={`${baseClassName}__font-group__container__button__font-name__container__body`} style={{ fontFamily: group.body }}>{group.body}</span>
 			</div>
-			{group.id === 1 && <span style={{ marginLeft: 'auto', textAlign: 'right' }}>Default</span>}
+			{group.id === 1 && <span className='default'>Default</span>}
 		</div>
 	</div>
 );
 
 const CustomFontsForm = ({ baseClassName, customFont, setCustomFont, handleCancelCustomFonts, handleApplyCustomFonts, renderFontOptions }) => (
-	<div style={{ width: '100%' }}>
+	<div className={`${baseClassName}__fonts-form__container`}>
 		<h5 className={`${baseClassName}__heading`}>CUSTOM FONTS</h5>
-		<div style={{ width: '100%' }}>
-			<div style={{ width: '100%' }}>
+		<div>
+			<div>
 				<div>
-					<span htmlFor="headings" style={{ margin: '10px 0px' }}>Headings</span><br />
+					<span htmlFor="headings">Headings</span><br />
 					<select
 						id="headings"
 						value={customFont.headings}
 						onChange={(e) => setCustomFont({ ...customFont, headings: e.target.value })}
-						style={{ margin: '5px 0px', width: '100%' }}
 					>
 						<option>select</option>
 						{renderFontOptions()}
 					</select>
 				</div>
 				<div>
-					<span htmlFor="body" style={{ margin: '10px 0px' }}>Body</span><br />
+					<span htmlFor="body">Body</span><br />
 					<select
 						id="body"
 						value={customFont.body}
 						onChange={(e) => setCustomFont({ ...customFont, body: e.target.value })}
-						style={{ margin: '5px 0px', width: '100%' }}
 					>
 						<option>select</option>
 						{renderFontOptions()}
 					</select>
 				</div>
 			</div>
-			<div style={{ marginTop: '20px', display: 'flex', justifyContent: 'flex-end' }}>
-				<Button onClick={() => handleCancelCustomFonts()} style={{ color: '#44494d' }}>
+			<div className={`${baseClassName}__fonts-form__container__button__container`} >
+				<Button className='cancel' onClick={() => handleCancelCustomFonts()} >
 					Cancel
 				</Button>
-				<Button onClick={handleApplyCustomFonts} variant="primary" style={{ backgroundColor: '#44494d', borderRadius: '5px' }}>
+				<Button className='apply' onClick={handleApplyCustomFonts} variant="primary" >
 					Apply
 				</Button>
 			</div>
@@ -57,38 +57,35 @@ const CustomFontsForm = ({ baseClassName, customFont, setCustomFont, handleCance
 );
 
 const CustomFontsDisplay = ({ baseClassName, selectedGroup, selectedCustomFont, handleGroupSelect, handleEditCustomFont }) => (
-	<div style={{ width: '100%' }}>
-		<div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+	<div className={`${baseClassName}__custom-fonts__container`}>
+		<div className={`${baseClassName}__custom-fonts__container__header`}>
 			<h5 className={`${baseClassName}__heading`}>
 				<span>CUSTOM FONTS</span>
 			</h5>
-			<button
-				onClick={() => handleEditCustomFont()}
-				style={{ background: 'none', border: 'none', padding: '0', textDecoration: 'underline', cursor: 'pointer', color: '#007cba' }}
-			>
+			<button onClick={() => handleEditCustomFont()} >
 				Edit fonts
 			</button>
 		</div>
 
 
-		<div style={{ marginBottom: '20px', display: 'flex', alignItems: 'flex-start', width: '100%' }}>
-			<div role="button" style={{ cursor: 'pointer', display: 'flex', alignItems: 'flex-start', marginLeft: '0px', width: '100%' }} onClick={() => handleGroupSelect('custom')}>
-				<Dashicon icon={'yes-alt'} size={30} style={{ marginRight: '10px', color: selectedGroup === 'custom' ? '#0160F0' : '#9ca2a7' }} />
-				<div style={{ marginLeft: '10px', flex: 1 }}>
-					<span style={{ fontSize: '18px', fontFamily: selectedCustomFont.headings, marginBottom: '5px' }}>{selectedCustomFont.headings}</span><br />
-					<span style={{ fontFamily: selectedCustomFont.body, marginBottom: '5px' }}>{selectedCustomFont.body}</span>
+		<div className={`${baseClassName}__font-group__container`}>
+			<div className={`${baseClassName}__font-group__container__button`} role="button" onClick={() => handleGroupSelect('custom')}>
+				<Dashicon className={`${baseClassName}__font-group__container__button__icon`} icon={'yes-alt'} size={30} style={{ color: selectedGroup === 'custom' ? '#0160F0' : '#9ca2a7' }} />
+				<div className={`${baseClassName}__font-group__container__button__font-name__container`} >
+					<span className={`${baseClassName}__font-group__container__button__font-name__container__heading`} style={{ fontFamily: selectedCustomFont.headings }}>{selectedCustomFont.headings}</span><br />
+					<span className={`${baseClassName}__font-group__container__button__font-name__container__body`} style={{ fontFamily: selectedCustomFont.body }}>{selectedCustomFont.body}</span>
 				</div>
 			</div>
 		</div>
 	</div>
 );
 
-const DesignFontsPanel = ({ baseClassName = 'nfd-onboarding-sidebar-customize--design-fonts-panel' }) => {
+const DesignFontsPanel = ({ baseClassName = 'nfd-onboarding-sidebar-customize__design-fonts-panel' }) => {
 	const fontGroups = [
-		{ id: 1, headings: 'BOTANICA', body: 'Inter' },
-		{ id: 2, headings: 'DM Sherif Display', body: 'DM Sans' },
-		{ id: 3, headings: 'Nunito', body: 'Nunito Sans' },
-		{ id: 4, headings: 'Oswald', body: 'Source Sans Pro' },
+		{ id: 1, headings: design.style.fonts_heading, body: design.style.fonts_content },
+		{ id: 2, headings: designStyles[1].fonts_heading, body: designStyles[1].fonts_content },
+		{ id: 3, headings: designStyles[2].fonts_heading, body: designStyles[2].fonts_content },
+		{ id: 4, headings: designStyles[3].fonts_heading, body: designStyles[3].fonts_content },
 	];
 
 	const [selectedGroup, setSelectedGroup] = useState(null);
@@ -97,7 +94,10 @@ const DesignFontsPanel = ({ baseClassName = 'nfd-onboarding-sidebar-customize--d
 	const [selectedCustomFont, setSelectedCustomFont] = useState(null);
 	const [isEditingCustomFont, setIsEditingCustomFont] = useState(false);
 
-	const fontOptions = ['Arial', 'Verdana', 'Courier New', 'Helvetica', 'Tahoma', 'Comic Sans MS'];
+	const fontsHeading = designStyles.map(style => style.fonts_heading);
+	const fontsContent = designStyles.map(style => style.fonts_content);
+
+	const fontOptions = [...new Set([...fontsHeading, ...fontsContent])];
 
 	const handleGroupSelect = (groupId) => {
 		setSelectedGroup(groupId);
@@ -133,7 +133,7 @@ const DesignFontsPanel = ({ baseClassName = 'nfd-onboarding-sidebar-customize--d
 
 	const renderFontGroups = () => {
 		return fontGroups.map((group) => (
-			<FontGroup key={group.id} group={group} selectedGroup={selectedGroup} handleGroupSelect={handleGroupSelect} />
+			<FontGroup baseClassName={baseClassName} key={group.id} group={group} selectedGroup={selectedGroup} handleGroupSelect={handleGroupSelect} />
 		));
 	};
 
@@ -165,7 +165,7 @@ const DesignFontsPanel = ({ baseClassName = 'nfd-onboarding-sidebar-customize--d
 	return (
 		<PanelBody className={baseClassName} initialOpen={true}>
 			<PanelRow>
-				<div className={`${baseClassName}__container`} style={{ width: '100%' }}>
+				<div className={`${baseClassName}__container`}>
 					<div className={`${baseClassName}__container__text`}>
 						<p className={`${baseClassName}__container__text__heading`}>
 							<strong>Fonts</strong>
@@ -177,15 +177,8 @@ const DesignFontsPanel = ({ baseClassName = 'nfd-onboarding-sidebar-customize--d
 
 			<PanelRow>
 				{!showCustomFonts && (
-					<div style={{ width: '100%', textAlign: 'center' }}>
+					<div className={`${baseClassName}__container`}>
 						<Button
-							style={{
-								border: '1px solid #9ca2a7',
-								borderRadius: '8px',
-								width: '100%',
-								display: 'block',
-								margin: '0 auto',
-							}}
 							onClick={() => {
 								setShowCustomFonts(true);
 								setIsEditingCustomFont(true);
