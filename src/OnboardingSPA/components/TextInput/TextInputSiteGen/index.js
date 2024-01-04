695f879ff7503c22ab9fe39f451e087934a41169
Allen Benny <allen.officially@gmail.com>
2024-01-04 10:59:06 +0530

    Optimize Code

--- src/OnboardingSPA/components/TextInput/TextInputSiteGen/index.js
+++ src/OnboardingSPA/components/TextInput/TextInputSiteGen/index.js
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import { useRef, useEffect, useState, memo } from '@wordpress/element';
-import { calculateAnalysisScore } from '../../../steps/SiteGen/SiteDetails/analyser';
 
 const TextInputSiteGen = ( {
 	hint,
@@ -23,23 +22,30 @@ const TextInputSiteGen = ( {
 		setIsValidInput( analysisResult >= 2 );
 	}, [ customerInput ] );
 
+	const calculateAnalysisScore = ( input ) => {
+		/* Number of Characters in the input
+		* Count < 100 => 0
+		* 100 < Count <= 150 => 1
+		* 150 < Count <= 200 => 2
+		* 200 < Count => 3
+		*/
+		const characterCount = input?.length;
+		let characterScore = 0;
+		if ( characterCount > 200 ) {
+			characterScore = 3;
+		} else if ( characterCount > 150 ) {
+			characterScore = 2;
+		} else if ( characterCount > 100 ) {
+			characterScore = 1;
+		}
+
+		return characterScore;
+	};
+
 	const addInputScoreSyling = ( num ) => {
 		const selectedButton = 'nfd-sg-input-box__info-icon--selected';
-		switch ( num ) {
-			case 1:
-				if ( num <= analysisScore ) {
-					return selectedButton;
-				}
-				break;
-			case 2:
-				if ( num <= analysisScore ) {
-					return selectedButton;
-				}
-				break;
-			case 3:
-				if ( num <= analysisScore ) {
-					return selectedButton;
-				}
+		if ( num <= analysisScore ) {
+			return selectedButton;
 		}
 	};
 
@@ -47,6 +53,21 @@ const TextInputSiteGen = ( {
 		e.preventDefault();
 		setCustomerInput( e.target.value );
 		setInputText( 'nfd-sg-input-box__field' );
+	};
+
+	const renderDetails = ( ) => {
+		const buttons = [];
+		for ( let i = 1; i <= 3; i++ ) {
+			buttons.push(
+				<div
+					className={ classNames(
+						'nfd-sg-input-box__info-icon',
+						addInputScoreSyling( i )
+					) }
+				/>
+			);
+		}
+		return buttons;
 	};
 
 	return (
@@ -68,24 +89,7 @@ const TextInputSiteGen = ( {
 						<div className={ 'nfd-sg-input-box__info-text' }>
 							Detail
 						</div>
-						<div
-							className={ classNames(
-								'nfd-sg-input-box__info-icon',
-								addInputScoreSyling( 1 )
-							) }
-						/>
-						<div
-							className={ classNames(
-								'nfd-sg-input-box__info-icon',
-								addInputScoreSyling( 2 )
-							) }
-						/>
-						<div
-							className={ classNames(
-								'nfd-sg-input-box__info-icon',
-								addInputScoreSyling( 3 )
-							) }
-						/>
+						{ renderDetails() }
 					</div>
 				) : (
 					<p className={ 'nfd-sg-input-box__hint' }>{ hint }</p>
