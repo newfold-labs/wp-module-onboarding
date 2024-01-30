9cf476feaead928387cb0e1fd8bba14ce21291bb
mr-vara <varaprasadmaruboina@gmail.com>
2024-01-30 18:10:09 +0530

    Reset the customized fonts and colors in SiteGen Preview  (#433)
    * Add Rest button to customize sidebar
    
    * Fix issue with updated homepages.data
    
    * Fix issues with lint
    
    * Fix lint

--- src/OnboardingSPA/components/Sidebar/components/Customize/Sidebar.js
+++ src/OnboardingSPA/components/Sidebar/components/Customize/Sidebar.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useRef } from 'react';
 import { Fill, PanelBody, PanelHeader, Button } from '@wordpress/components';
 import { Fragment, memo, Suspense } from '@wordpress/element';
 import { closeSmall } from '@wordpress/icons';
@@ -9,6 +11,7 @@ import { SIDEBAR_SLOTFILL_PREFIX } from '../../../../../constants';
 import SidebarSkeleton from './Skeleton/SidebarSkeleton';
 
 const CustomizeSidebar = () => {
+	const sidebarRef = useRef();
 	const { currentStep } = useSelect( ( select ) => {
 		return {
 			currentStep: select( nfdOnboardingStore ).getCurrentStep(),
@@ -20,6 +23,11 @@ const CustomizeSidebar = () => {
 	const closeSideBar = () => {
 		setIsSidebarOpened( false );
 	};
+
+	const resetCustomization = () => {
+		sidebarRef.current.resetCustomizationCallback();
+	};
+
 	return (
 		<Fill name={ `${ SIDEBAR_SLOTFILL_PREFIX }/Customize` }>
 			<PanelBody
@@ -33,6 +41,7 @@ const CustomizeSidebar = () => {
 							'wp-module-onboarding'
 						) }
 					>
+						<Button onClick={ resetCustomization }>Reset</Button>
 						<div className="nfd-onboarding-sidebar-learn-more__header">
 							<Button
 								className="nfd-onboarding-sidebar-learn-more__header__icon"
@@ -46,7 +55,7 @@ const CustomizeSidebar = () => {
 							( SidebarComponent, index ) => {
 								return (
 									<Fragment key={ index }>
-										<SidebarComponent />
+										<SidebarComponent ref={ sidebarRef } />
 									</Fragment>
 								);
 							}
