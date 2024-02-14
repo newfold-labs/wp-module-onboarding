25ff94e7519c4c8dd47db41c127397147ace049e
ajayadav09 <ajayadav09@gmail.com>
2024-02-14 17:37:43 +0530

    fix linting

--- src/OnboardingSPA/steps/SiteGen/Editor/Header/index.js
+++ src/OnboardingSPA/steps/SiteGen/Editor/Header/index.js
@@ -319,9 +319,9 @@ const StepSiteGenEditorHeader = () => {
 						>
 							{ isLargeViewport
 								? __(
-										'Save & Continue',
-										'wp-module-onboarding'
-								  )
+									'Save & Continue',
+									'wp-module-onboarding'
+								)
 								: __( 'Next', 'wp-module-onboarding' ) }
 						</div>
 						{ isSaving ? (
