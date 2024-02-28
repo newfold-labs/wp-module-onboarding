5ad08bdcdbfdf16b2544734e6e5dab86bfbefe8e
ajayadav09 <ajayadav09@gmail.com>
2024-02-28 16:44:58 +0530

    removed build step from features

--- src/OnboardingSPA/chapters/siteGen/features.js
+++ src/OnboardingSPA/chapters/siteGen/features.js
@@ -2,10 +2,9 @@ import { __ } from '@wordpress/i18n';
 
 import { CHAPTER_SITEGEN_FEATURES } from '../../../constants';
 import { Chapter } from '../../data/models/Chapter';
-import { stepSiteGenBuilding } from '../../steps/SiteGen/Building/step';
 import { stepSiteGenExperience } from '../../steps/SiteGen/Experience/step';
 
-const steps = [ stepSiteGenExperience, stepSiteGenBuilding ];
+const steps = [ stepSiteGenExperience ];
 
 export const siteGenFeatures = new Chapter( {
 	id: CHAPTER_SITEGEN_FEATURES,
