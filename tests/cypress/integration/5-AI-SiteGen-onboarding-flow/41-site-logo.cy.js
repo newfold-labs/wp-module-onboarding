c1ec81cf7e7810f278c7c73470cf49c26d8a4ea6
sangeetha-nayak <sangeetha.nayak@newfold.com>
2024-03-27 16:53:16 +0530

    structured fixtures and mock logic

--- tests/cypress/integration/5-AI-SiteGen-onboarding-flow/41-site-logo.cy.js
+++ tests/cypress/integration/5-AI-SiteGen-onboarding-flow/41-site-logo.cy.js
@@ -1,14 +1,17 @@
-describe('SiteGen Site Logo Step', function () {
-    const end_point = '/index.php?rest_route=%2Fnewfold-onboarding%2Fv1%2Fsitegen%2Fgenerate&flow=sitegen&_locale=user'
-    before(() => {
-        // cy.visit(
-        //     'wp-admin/?page=nfd-onboarding#/sitegen/step/site-logo'
-        // );
-    });
+const fix = require('../../fixtures/example.json');
+import { apiList, siteGenMockAll } from '../wp-module-support/MockApi.cy';
 
-  it('Check whether API mocking is possible for site_config', () => {
+describe('SiteGen Site Logo Step', function () {
+  const end_point = '/index.php?rest_route=%2Fnewfold-onboarding%2Fv1%2Fsitegen%2Fgenerate&flow=sitegen&_locale=user'
+  // before(() => {
+  //     // cy.visit(
+  //     //     'wp-admin/?page=nfd-onboarding#/sitegen/step/site-logo'
+  //     // );
+  // });
+
+  it.only('Check whether API mocking is possible for site_config', () => {
     const site_config_fixture = `vendor/newfold-labs/wp-module-onboarding/tests/cypress/fixtures/site-config.json`
-    const site_classification_fixture = `vendor/newfold-labs/wp-module-onboarding/tests/cypress/fixtures/site-classification.json`
+    const site_classification_fixture = `vendor/newfold-labs/wp-module-onboarding/tests/cypress/fixtures/example`
     const target_audience_fixture = `vendor/newfold-labs/wp-module-onboarding/tests/cypress/fixtures/target-audience.json`
     // // ***************       START OF LOGIC 1 ****************************************
     // cy.intercept(end_point)
@@ -33,46 +36,50 @@ describe('SiteGen Site Logo Step', function () {
     // *********************** END OF LOGIC 1 **********************************
 
     // // *****************START OF LOGIC 2 *************************************
+    // cy.fixture(site_classification_fixture).as('siteClassJson')
     // Intercepting requests to the same endpoint
-cy.intercept(end_point, (req) => {
-  // Check the request object and provide appropriate mock response
-  if (req.hasOwnProperty('identifier') && req.identifier.includes('site_config')) {
-    cy.log('********************************************************')
+    cy.intercept(apiList.sitegen, (req) => {
+      siteGenMockAll(req)
 
-    req.reply({
-      statusCode: 200,
-      body: site_config_fixture,
-      headers: {
-        'content-type': 'application/json'
-      }
-    });
-  }
-  // if (req.hasOwnProperty('identifier') && req.identifier.includes('site_classification')) {
-  //   cy.log(JSON.stringify(req.body))
-  //   req.reply({
-  //     statusCode: 200,
-  //     body: site_classification_fixture,
-  //     headers: {
-  //       'content-type': 'application/json'
-  //     }
-  //   });
-  // } 
 
-  // if (req.hasOwnProperty('identifier') && req.identifier.includes('target_audience')) {
-  //   cy.log(JSON.stringify(req.body))
-  //   req.reply({
-  //     statusCode: 200,
-  //     body: target_audience_fixture,
-  //     headers: {
-  //       'content-type': 'application/json'
-  //     }
-  //   });
-  // } 
+      // const requestBody = req.body
+      
+      // Check the request object and provide appropriate mock response
+      // if (requestBody.hasOwnProperty('identifier') && requestBody.identifier == 'site_config') {
 
-  // similarly for other identifiers
-}).as('sitegenCalls');
+      //   req.reply({
+      //     statusCode: 200,
+      //     body: fix,
+      //     headers: {
+      //       'content-type': 'application/json'
+      //     }
+      //   });
+      // }
+      // if ( requestBody.hasOwnProperty('identifier') && requestBody.identifier == 'site_classification') {
+      //   req.reply({
+      //     statusCode: 200,
+      //     body: { "justtest": "yes" },
+      //     headers: {
+      //       'content-type': 'application/json'
+      //     }
+      //   });
+      // } 
+
+      // if (req.hasOwnProperty('identifier') && req.identifier.includes('target_audience')) {
+      //   cy.log(JSON.stringify(req.body))
+      //   req.reply({
+      //     statusCode: 200,
+      //     body: target_audience_fixture,
+      //     headers: {
+      //       'content-type': 'application/json'
+      //     }
+      //   });
+      // }
+
+      // similarly for other identifiers
+    }).as('sitegenCalls');
     
-      cy.visit('wp-admin/index.php?page=nfd-onboarding#/sitegen/step/site-logo')
+    cy.visit('wp-admin/index.php?page=nfd-onboarding#/sitegen/step/site-logo')
     
     cy.wait('@sitegenCalls', { timeout: 25000 })
     
@@ -81,5 +88,4 @@ cy.intercept(end_point, (req) => {
 
 
   });
-    
 });
