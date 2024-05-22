50df3ca7140da6f29910d5db734f3a4f8158214e
ajayadav09 <ajayadav09@gmail.com>
2024-05-22 11:36:28 +0530

    updated the tests

--- tests/cypress/integration/wp-module-support/siteGen.cy.js
+++ tests/cypress/integration/wp-module-support/siteGen.cy.js
@@ -6,25 +6,6 @@ export const AdminBarCheck = () => {
 	);
 };
 
-/* export const DarkBGCheck = () => {
-	cy.wait( 2000 );
-	// When the page loads, it should have dark background by default
-	cy.get( '.nfd-onboarding-sitegen-dark' ).should( 'be.visible' );
-};
-
-export const LightBGCheck = () => {
-	cy.get( '.nfd-onboarding-toggle__theme__button__dark' )
-		.should( 'exist' )
-		.click();
-	cy.get( '.nfd-onboarding-sitegen-light' ).should( 'be.visible' );
-	// Now changing the background back to dark
-	cy.get( '.nfd-onboarding-toggle__theme__button__light' )
-		.should( 'exist' )
-		.click();
-	cy.get( '.nfd-onboarding-sitegen-dark' ).should( 'be.visible' );
-};
- */
-
 export const LightBGCheck = () => {
 	cy.wait( 2000 );
 	// When the page loads, it should have light background by default
