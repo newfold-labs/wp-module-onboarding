7316c32d24c93914871233f560f2182d953e6119
lokapure.girish <lokapure.girish@gmail.com>
2024-07-22 10:28:50 +0530

    removing socialmedia steps from sitegen chapter

--- src/OnboardingSPA/chapters/sitegen.js
+++ src/OnboardingSPA/chapters/sitegen.js
@@ -3,14 +3,12 @@ import { Chapter } from '../data/models/Chapter';
 import { __ } from '@wordpress/i18n';
 import { stepSiteGenSiteDetails } from '../steps/SiteGen/SiteDetails/step';
 import { stepSiteGenSiteLogo } from '../steps/SiteGen/SiteLogo/step';
-import { stepSiteGenSocialMedia } from '../steps/SiteGen/SocialMedia/step';
 import { stepSiteGenExperience } from '../steps/SiteGen/Experience/step';
 import { stepSiteGenPreview } from '../steps/SiteGen/Preview/step';
 import { stepSiteGenEditor } from '../steps/SiteGen/Editor/step';
 
 const steps = [
 	stepSiteGenSiteDetails,
-	stepSiteGenSocialMedia,
 	stepSiteGenSiteLogo,
 	stepSiteGenExperience,
 	stepSiteGenPreview,
