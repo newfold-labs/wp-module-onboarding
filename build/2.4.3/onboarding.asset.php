ae80bb72b8772ef7c630f978971ddf22a32c1cea
Allen Benny <allen.officially@gmail.com>
2024-10-23 13:58:13 +0530

    Remove 2.4.3 build

--- build/2.4.3/onboarding.asset.php
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:build/2.5.0/onboarding.asset.php
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-deprecated', 'wp-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-preferences', 'wp-primitives', 'wp-style-engine', 'wp-url'), 'version' => '88931818b5dfc5a6243b');
-========
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-deprecated', 'wp-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-preferences', 'wp-primitives', 'wp-style-engine', 'wp-url'), 'version' => '9773cf27d9013f036b80');
->>>>>>>> trunk:build/2.4.3/onboarding.asset.php
--- build/2.4.3/sitegen-theme-marker.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '6db690db53b5e616aafe');
