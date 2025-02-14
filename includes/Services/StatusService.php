20fbab035e68a9db7ff7f3fc7d9f946eda03ca67
Allen Benny <allen.officially@gmail.com>
2025-02-14 13:25:31 +0530

    Update StatusService.php

--- includes/Services/StatusService.php
+++ includes/Services/StatusService.php
@@ -44,43 +44,54 @@ class StatusService {
 	 * @return bool True if the site was created within the last 275 days, false otherwise.
 	 */
 	private static function is_site_created_within_last_9_months() {
-		$install_date_timestamp = get_option(Options::get_option_name( 'bluehost_plugin_install_date', false ));
-		
+		$install_date_timestamp = get_option( Options::get_option_name( 'bluehost_plugin_install_date', false ) );
+
 		// If the option is not set or is invalid, return false
-		if (!$install_date_timestamp) {
+		if ( ! $install_date_timestamp ) {
 			return false;
 		}
 
 		// Calculate the timestamp for 275 days ago (9 months)
-		$nine_months_ago = time() - (275 * 24 * 60 * 60);
-		
+		$nine_months_ago = time() - ( 275 * 24 * 60 * 60 );
+
 		return $install_date_timestamp >= $nine_months_ago;
 	}
 
 	/**
-	 * Determines if a user is eligible for Restarting Onboarding again
+	 * Determines if a user is eligible for Restarting Onboarding again.
 	 * Toggles a wp_option to reflect the user's eligibility status.
 	 *
 	 * @return void
 	 */
 	public static function determine_onboarding_restart_eligibility() {
-		\do_action('qm/debug', self::is_site_created_within_last_9_months());
-		return;
-		
-		//Check if the brand is eligible for Restarting Onboarding
-		if(isset(Brands::get_brands()[NFD_ONBOARDING_PLUGIN_BRAND]['config']['canRestartOnboarding']) || false == Brands::get_brands()[NFD_ONBOARDING_PLUGIN_BRAND]['config']['canRestartOnboarding']){
+
+		// Check if the brand is eligible for Restarting Onboarding
+		$brand_config = Brands::get_brands()[ NFD_ONBOARDING_PLUGIN_BRAND ]['config'] ?? array();
+		if ( empty( $brand_config['canRestartOnboarding'] ) || ! $brand_config['canRestartOnboarding'] ) {
 			return;
 		}
 
-		//Check if hasAISiteGen Capability is active and is created in the last 9 months
-		if(false == Config::has_ai_sitegen() || false == self::is_site_created_within_last_9_months()){
+		// Check if AI SiteGen Hiive capability is active and the site was created in the last 9 months
+		if ( ! Config::has_ai_sitegen() || ! self::is_site_created_within_last_9_months() ) {
 			return;
 		}
 
-		// Get Flow value and check if Total Onboarding Tries is less than 3
-		$flow_data = get_option( Options::get_option_name( 'flow' ), false );
-		if(isset($flow_data)){
-			$totalOnboardingTries = $flow_data['totalOnboardingTries'];
+		// Get flow data and check if the total onboarding tries are less than or equal to 3
+		$flow_data = get_option( Options::get_option_name( 'flow' ) );
+
+		if ( isset( $flow_data ) ) {
+			// Increment the total onboarding tries
+			$flow_data['onboardingRetries']['retryCount'] = ( $flow_data['onboardingRetries']['retryCount'] ?? 0 ) + 1;
+
+			// Update the flow data with the incremented total onboarding tries count
+			update_option( Options::get_option_name( 'flow' ), $flow_data );
+
+			// Determine eligibility for restarting onboarding
+			$currentRetryCount = $flow_data['onboardingRetries']['retryCount'];
+			$canRestart        = $currentRetryCount <= $flow_data['onboardingRetries']['maxRetryCount'];
+
+			// Update the eligibility status in wp_option
+			update_option( Options::get_option_name( 'can_restart' ), $canRestart );
 		}
 	}
 
