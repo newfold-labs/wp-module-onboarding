3698a2a3b68bda2718b4afac5f1587a873d93310
Allen Benny <allen.officially@gmail.com>
2025-02-19 11:14:32 +0530

    Update use-global-styles-output.js

--- src/OnboardingSPA/utils/global-styles/use-global-styles-output.js
+++ src/OnboardingSPA/utils/global-styles/use-global-styles-output.js
@@ -516,20 +516,20 @@ export function getLayoutStyles( {
 								combinedSelector =
 									selector === ROOT_BLOCK_SELECTOR
 										? `:where(.${ className }${
-												spacingStyle?.selector || ''
-										  })`
+											spacingStyle?.selector || ''
+										})`
 										: `:where(${ selector }.${ className }${
-												spacingStyle?.selector || ''
-										  })`;
+											spacingStyle?.selector || ''
+										})`;
 							} else {
 								combinedSelector =
 									selector === ROOT_BLOCK_SELECTOR
 										? `:root :where(.${ className })${
-												spacingStyle?.selector || ''
-										  }`
+											spacingStyle?.selector || ''
+										}`
 										: `:root :where(${ selector }-${ className })${
-												spacingStyle?.selector || ''
-										  }`;
+											spacingStyle?.selector || ''
+										}`;
 							}
 							ruleset += `${ combinedSelector } { ${ declarations.join(
 								'; '
