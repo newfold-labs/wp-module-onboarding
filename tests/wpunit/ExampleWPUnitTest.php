a726e2a581df45f5a1a0a9634af1bb02a7a1879e
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-03-21 16:10:49 +0000

    PHPUnit scaffolding

---
+++ tests/wpunit/ExampleWPUnitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace NewfoldLabs\WP\Module\Onboarding;
+
+/**
+ * @coversDefaultClass \NewfoldLabs\WP\Module\Onboarding\Example
+ */
+class ExampleWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase {
+
+	public function test_is_true() :void
+	{
+
+		$this->assertTrue( true );
+	}
+
+}
