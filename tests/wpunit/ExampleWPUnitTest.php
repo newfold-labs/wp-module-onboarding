9632f10eeaac765bf735b004af01aa82eabb35d5
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-03-21 16:20:11 +0000

    remove unit test

--- tests/wpunit/ExampleWPUnitTest.php
+++ tests/wpunit/ExampleWPUnitTest.php
@@ -6,11 +6,5 @@ namespace NewfoldLabs\WP\Module\Onboarding;
  * @coversDefaultClass \NewfoldLabs\WP\Module\Onboarding\Example
  */
 class ExampleWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase {
-
-	public function test_is_true() :void
-	{
-
-		$this->assertTrue( true );
-	}
-
+	
 }
