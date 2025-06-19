d534b82b226aec1b681ff3c331bd9d72a25444ff
zlateska <hzlateska@gmail.com>
2025-06-19 09:21:36 -0400

    Fix console errors & warnings and missing imports

--- src/app/steps/Intake/LanguageSelector.js
+++ src/app/steps/Intake/LanguageSelector.js
@@ -1,27 +1,25 @@
 import { SelectField } from '@newfold/ui-component-library';
 import { useSelect } from '@wordpress/data';
+import { useState, useMemo } from '@wordpress/element';
 import { nfdOnboardingStore } from '@/data/store';
 
 const LanguageSelector = ( { value, onChange } ) => {
 	const [ selectedLanguage, setSelectedLanguage ] = useState( null );
 	// Get languages data.
-	const { languages } = useSelect( ( select ) => {
-		return {
-			languages: select( nfdOnboardingStore ).getLanguages(),
-		};
-	} );
+	const languages = useSelect( ( select ) => {
+		return select( nfdOnboardingStore ).getLanguages();
+	}, [] );
 
-	// Get the initial language.
-	const getInitialLanguage = () => {
-		let initialLanguage = null;
+	const initialLanguage = useMemo( () => {
+		let lang = null;
 		if ( value ) {
-			initialLanguage = languages.find( ( language ) => language.locale === value );
+			lang = languages.find( ( language ) => language.locale === value );
 		}
-		if ( ! initialLanguage ) {
-			initialLanguage = languages.find( ( language ) => language.is_default );
+		if ( ! lang ) {
+			lang = languages.find( ( language ) => language.is_default );
 		}
-		return initialLanguage;
-	};
+		return lang;
+	}, [ languages, value ] );
 
 	const handleLanguageChange = ( updatedValue ) => {
 		const updatedLanguage = languages.find( ( language ) => language.locale === updatedValue );
@@ -29,23 +27,26 @@ const LanguageSelector = ( { value, onChange } ) => {
 		onChange( updatedLanguage.locale );
 	};
 
-	const languageOptions = languages.map( ( language ) => {
-		return (
-			<SelectField.Option
-				key={ language.locale }
-				value={ language.locale }
-				label={ language.native_name }
-			/>
-		);
-	} );
+	const languageOptions = useMemo( () => {
+		return languages.map( ( language ) => {
+			return (
+				<SelectField.Option
+					key={ language.locale }
+					value={ language.locale }
+					label={ language.native_name }
+				/>
+			);
+		} );
+	}, [ languages ] );
 
 	return (
 		<div className="nfd-w-[40%]">
 			<SelectField
 				id="nfd-onboarding-language-selector"
+				name="nfd-onboarding-language-selector"
 				label={ __( 'Language', 'wp-module-onboarding' ) }
-				value={ selectedLanguage ? selectedLanguage.locale : getInitialLanguage().locale }
-				selectedLabel={ selectedLanguage ? selectedLanguage.native_name : getInitialLanguage().native_name }
+				value={ selectedLanguage ? selectedLanguage.locale : initialLanguage.locale }
+				selectedLabel={ selectedLanguage ? selectedLanguage.native_name : initialLanguage.native_name }
 				onChange={ handleLanguageChange }
 			>
 				{ languageOptions }
