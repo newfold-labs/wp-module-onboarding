4ff604b7e2b49b5579c6591d30af85c58823a37a
Abhijit Bhatnagar <abhijit.bhatnagar@gmail.com>
2025-06-21 06:49:42 +0530

    lint fixes

--- src/app/steps/Migration/MigrationStep.js
+++ src/app/steps/Migration/MigrationStep.js
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from '@wordpress/element';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { Container, Title, Spinner } from '@newfold/ui-component-library';
 import { ExclamationCircleIcon } from '@heroicons/react/24/solid';
@@ -25,6 +25,7 @@ const MigrationStep = () => {
 
 	/**
 	 * Track migration initiated event
+	 * 
 	 * @param { string } instaWpMigrationUrl The migration url
 	 * @return { Promise<void> }
 	 */
