db01b940cc3ccf0197ab1e2fc02a50165ffabd7f
wpalani <abdulrahmanalani95@gmail.com>
2025-06-23 10:30:11 -0700

    Generate site pages during site generation process

--- src/app/utils/sitegen/generateSite.js
+++ src/app/utils/sitegen/generateSite.js
@@ -13,11 +13,8 @@ const generateSite = async () => {
 		return false;
 	}
 
-	/**
-	 * Fire and forget: Generate the rest of the site pages.
-	 * This operation can take a while and it results don't impact the state of the application.
-	 */
-	generateSitePages().catch( () => {} );
+	// Generate the rest of the site pages.
+	await generateSitePages();
 
 	return true;
 };
