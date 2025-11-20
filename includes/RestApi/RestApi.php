01808c277ba0abb2ab1dcd6933288881efbd2b73
wpalani <abdulrahmanalani95@gmail.com>
2025-11-20 16:49:28 -0700

    Remove languages controller

--- includes/RestApi/RestApi.php
+++ includes/RestApi/RestApi.php
@@ -33,7 +33,6 @@ final class RestApi {
 		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\SiteClassificationController',
 		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\SiteGenController',
 		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\PreviewsController',
-		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\LanguagesController',
 		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\DesignController',
 		'NewfoldLabs\\WP\\Module\\Onboarding\\RestApi\\GlobalStylesController',
 	);
