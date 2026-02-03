c732cedf2c89d65ef56de0459001bce59749c0b5
Evan Mullins <evanrm@gmail.com>
2026-02-03 11:46:23 -0500

    fix wpunit test failure

--- tests/wpunit/_bootstrap.php
+++ tests/wpunit/_bootstrap.php
@@ -8,3 +8,9 @@
 $module_root = dirname( dirname( __DIR__ ) );
 
 require_once $module_root . '/vendor/autoload.php';
+
+// Required by onboarding-data when RestApi/RestApiFilter run (e.g. Data::current_brand()).
+// Normally set by Brands::set_current_brand() when the module is registered with the container.
+if ( ! defined( 'NFD_ONBOARDING_PLUGIN_BRAND' ) ) {
+	define( 'NFD_ONBOARDING_PLUGIN_BRAND', 'bluehost' );
+}
