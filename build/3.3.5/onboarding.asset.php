eb9ea2ec4540b2ebae22ea1a9df2cda3020b5a1f
wpalani <abdulrahmanalani95@gmail.com>
2026-04-16 08:55:48 -0700

    build assets

--- build/3.3.5/onboarding.asset.php
+++ build/3.3.5/onboarding.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-deprecated', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-preferences', 'wp-url'), 'version' => 'af328688d1fc4efc674b');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-deprecated', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-preferences', 'wp-url'), 'version' => '2dc74a0d617fa94fa82e');
