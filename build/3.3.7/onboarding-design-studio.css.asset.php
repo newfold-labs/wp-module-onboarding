77e2236bc2f8973adb8d1e55bedf31675d2d3bea
Evan Mullins <evanrm@gmail.com>
2026-07-22 17:17:22 -0400

    prep for release - 3.3.6 → 3.3.7

---
+++ build/3.3.7/onboarding-design-studio.css.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2a8551354c5b4fe4049e');
---
+++ build/3.3.7/sitegen-theme-marker.css.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";let e={};(e=>{Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})})(e),((window.newfold=window.newfold||{}).Onboarding=window.newfold.Onboarding||{})["sitegen-theme-marker.css"]=e})();
